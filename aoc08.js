66c604a5341d2c70b78b9c384276a24f2e263533
monk-time <monk-time@users.noreply.github.com>
2015-12-08 14:00:47 +0300

    Fixes an edge case

--- aoc08.js
+++ aoc08.js
@@ -2,8 +2,9 @@
 let input = document.body.textContent.trim().split('\n'),
     [esc, char] = [/\\(?:"|\\)/g, /\\x[0-9a-f]{2}/g],
     matches = (re, s) => (s.match(re) || []).length,
-    diff1 = s => 2 +     matches(esc, s) + 3 * matches(char, s),
-    diff2 = s => 4 + 2 * matches(esc, s) +     matches(char, s),
-    countAll = (arr, f) => arr.map(f).reduce((a, b) => a + b);
+    clean = s => s.replace(esc, 'X'), // for edge cases like "\\xce"
+    diff1 = s => 2 +     matches(esc, s) + 3 * matches(char, clean(s)),
+    diff2 = s => 4 + 2 * matches(esc, s) +     matches(char, clean(s)),
+    countAll = (f, arr) => arr.reduce((a, b) => a + f(b), 0);
 
-console.log([diff1, diff2].map(f => countAll(input, f)));
+console.log([diff1, diff2].map(f => countAll(f, input)));
