298243b169932f0ab0cbc8382270ab86659ff7d0
monk-time <monk-time@users.noreply.github.com>
2015-12-15 21:15:59 +0300

    Extracted array sum function

--- aoc15.js
+++ aoc15.js
@@ -1,13 +1,15 @@
 'use strict';
+let sum = arr => arr.reduce((a, b) => a + b, 0);
+
 // splitN(100, 3) === [[1, 1, 98], [1, 2, 97], ..., [97, 2, 1], [98, 1, 1]]
 function* splitN(n, parts, res = []) {
-    let partialSum = res.reduce((a, b) => a + b, 0);
+    let partialSum = sum(res);
     for (let i = 1; i < n - partialSum - (parts - 1) + 1; i++) {
         let resNew = [...res, i];
         if (parts > 2) {
             yield* splitN(n, parts - 1, resNew);
         } else {
-            resNew.push(n - resNew.reduce((a, b) => a + b, 0));
+            resNew.push(n - partialSum - i);
             yield resNew;
         }
     }
@@ -19,13 +21,13 @@ function maxScore(ingrs, spoonNum, calValue) {
         let ingrsAll = ingrs.map((ingr, i) => ingr.map(prop => prop * spoons[i])),
             cal = ingrsAll.map(ingr => ingr.pop()); // mutates ingrsAll
         if (calValue) {
-            if (calValue !== cal.reduce((a, b) => a + b)) {
+            if (calValue !== sum(cal)) {
                 continue;
             }
         }
 
-        let sum = ingrsAll.reduce((a, b) => a.map((prop, i) => prop + b[i])),
-            score = sum.reduce((acc, prop) => acc * Math.max(prop, 0), 1);
+        let propSum = ingrsAll.reduce((a, b) => a.map((prop, i) => prop + b[i])),
+            score = propSum.reduce((acc, prop) => acc * Math.max(prop, 0), 1);
         if (score > max) {
             max = score;
         }
