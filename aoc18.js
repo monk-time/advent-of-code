134bf734f9bd31d549bfacffdbc6395259f1e733
monk-time <monk-time@users.noreply.github.com>
2015-12-18 13:40:31 +0300

    Improved perf. (~19 sec)

--- aoc18.js
+++ aoc18.js
@@ -1,37 +1,37 @@
 'use strict';
-let inRange  = (dim, i, j) => i >= 0 && j >= 0 && i < dim && j < dim,
-    isCorner = (dim, i, j) => (i === 0 || i === dim - 1) && (j === 0 || j === dim - 1),
-    getSafe  = (arr, i, j) => inRange(arr.length, i, j) && arr[i][j],
+function parse(input) {
+    let grid = input.split('\n').map(s => ['.', ...s, '.'].map(ch => ch === '#')),
+        dim = grid.length,
+        emptyRow = dim => [...new Array(dim + 2)].fill(0);
+    return [emptyRow(dim), ...grid, emptyRow(dim)];
+}
+
+let isCorner = (dim, i, j) => (i === 1 || i === dim - 2) && (j === 1 || j === dim - 2),
     nbsDelta = [
         [-1, -1], [-1, 0], [-1, 1],
         [ 0, -1],          [ 0, 1],
         [ 1, -1], [ 1, 0], [ 1, 1]
     ],
     nbs = (i, j) => nbsDelta.map(([di, dj]) => [i + di, j + dj]),
-    countTruthy = arr => arr.filter(x => x).length,
-    countNbs = (arr, i, j, cornersOn) => {
-        let isOn = ([x, y]) => (cornersOn && isCorner(arr.length, x, y)) || getSafe(arr, x, y);
-        return countTruthy(nbs(i, j).map(isOn));
-    };
+    countTruthy = arr => arr.filter(x => x).length;
 
 function update(grid, cornersOn = false) {
     let gridNew = grid.map(row => row.slice()),
-        dim = grid.length; // grid must be square
-    for (let i = 0; i < dim; i++) {
-        for (let j = 0; j < dim; j++) {
-            let on = countNbs(grid, i, j, cornersOn);
-            gridNew[i][j] = (cornersOn && isCorner(dim, i, j)) ||
-                (grid[i][j] ? (on === 2 || on === 3) : on === 3);
+        dim = grid.length, // grid must be square
+        isOn = ([x, y]) => cornersOn && isCorner(dim, x, y) || grid[x][y];
+    for (let i = 1; i < dim - 1; i++) {
+        for (let j = 1; j < dim - 1; j++) {
+            let on = countTruthy(nbs(i, j).map(isOn));
+            gridNew[i][j] = cornersOn && isCorner(dim, i, j) ||
+                (grid[i][j] ? on === 2 || on === 3 : on === 3);
         }
     }
 
     return gridNew;
 }
 
-let input = document.body.textContent.trim(),
-    grid = input.split('\n').map(s => [...s].map(ch => ch === '#')),
-    countAll = arr => arr.reduce((sum, row) => sum + countTruthy(row), 0),
-    nSteps = (n, corners) => [...new Array(n)].reduce(x => update(x, corners), grid);
+let grid = parse(document.body.textContent.trim()),
+    nSteps = (n, corners) => [...new Array(n)].reduce(x => update(x, corners), grid),
+    countAll = arr => arr.reduce((sum, row) => sum + countTruthy(row), 0);
 
-console.log(countAll(nSteps(100)), countAll(nSteps(100, true)));
-
+console.log(countAll(nSteps(10)), countAll(nSteps(10, true)));
