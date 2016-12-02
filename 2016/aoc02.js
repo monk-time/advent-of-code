bd8f654f69f2dc790a481f6c21c0e835bb06e5f5
monk-time <monk-time@users.noreply.github.com>
2016-12-02 12:04:57 +0300

    Day 02: Shorten

--- 2016/aoc02.js
+++ 2016/aoc02.js
@@ -6,21 +6,18 @@
         ['123', '456', '789'],
         ['  1  ', ' 234 ', '56789', ' ABC ', '  D  ']
     ];
-    const code = (keypad, instructions, start)  => {
-        const correctKey = (i, j) => keypad[i][j] !== ' ' ? [i, j] : null,
-            move = (pred, f) => (i, j) => pred(i, j) && correctKey(...f(i, j)) || [i, j];
-        const moves = {
-            U: move((i, _) => i > 0,                    (i, j) => [i - 1, j]),
-            D: move((i, _) => i < keypad.length - 1,    (i, j) => [i + 1, j]),
-            L: move((_, j) => j > 0,                    (i, j) => [i, j - 1]),
-            R: move((_, j) => j < keypad[0].length - 1, (i, j) => [i, j + 1])
-        };
+    const delta = { U: [-1, 0], D: [1, 0], L: [0, -1], R: [0, 1] };
+    const code = (keypad, instructions, key0)  => {
         const key = ([i, j]) => keypad[i][j],
-            coords = ch => keypad.map((s, i) => [i, s.indexOf(ch)]).find(a => a[1] > -1),
-            btn = (s, coord) => s.split('').reduce(([i, j], m) => moves[m](i, j), coord);
+            point = k => keypad.map((s, i) => [i, s.indexOf(k)]).find(p => p[1] > -1);
+        const shift = (dir, p) => p.map(
+            (n, i) => Math.min(Math.max(n + delta[dir][i], 0), keypad.length - 1));
+        const validate = p => key(p) !== ' ' ? p : null,
+            move = (dir, p) => validate(shift(dir, p)) || p,
+            findBtn = (s, p0) => s.split('').reduce((p, dir) => move(dir, p), p0);
 
         return instructions.reduce(
-            (c, s) => c + key(btn(s, coords(c[c.length - 1]))), start
+            (c, s) => c + key(findBtn(s, point(c[c.length - 1]))), key0
         ).slice(1);
     };
 
