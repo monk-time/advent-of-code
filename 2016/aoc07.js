06876fdd6f66ac6a92a020077b2d3255b171fa82
monk-time <monk-time@users.noreply.github.com>
2016-12-07 12:42:44 +0300

    Day 07: Match only group 1 with a longer regex

--- 2016/aoc07.js
+++ 2016/aoc07.js
@@ -3,15 +3,14 @@
 {
     const [supernet, hypernet] = [0, 1].map(mod =>
         ip => ip.split(/\[(.+?)\]/g).filter((_, i) => i % 2 === mod).join('-'));
-    const matchAll = function* (re, s, m = re.exec(s)) { // overlapping matches
+    const matchAll = function* (re, s, m = re.exec(s)) { // all group 1 matches
         if (m === null) return;
-        yield m[0];
-        re.lastIndex -= m[0].length - 1;
+        yield m[1];
         yield* matchAll(re, s);
     };
     const input = document.body.textContent.trim().split('\n'),
         abbas = s => s.match(/(.)((?!\1).)\2\1/g) || [],
-        abas = s => [...matchAll(/(.)(?!\1).(?=\1)/g, s)],
+        abas = s => [...matchAll(/(?=((.)(?!\2).)\2)./g, s)],
         hasTLS = ip => abbas(supernet(ip)).length > 0 && abbas(hypernet(ip)).length === 0,
         hasSSL = ip => abas(supernet(ip)).some(s => abas(hypernet(ip)).includes(s[1] + s[0]));
 
