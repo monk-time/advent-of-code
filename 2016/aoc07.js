51603892ff799d20c9522568687fe29aee1e0c28
monk-time <monk-time@users.noreply.github.com>
2016-12-07 12:02:13 +0300

    Day 07: Full matches with manual RegExp rewinding

--- 2016/aoc07.js
+++ 2016/aoc07.js
@@ -3,14 +3,15 @@
 {
     const [supernet, hypernet] = [0, 1].map(mod =>
         ip => ip.split(/\[(.+?)\]/g).filter((_, i) => i % 2 === mod).join('-'));
-    const matchEnds = function* (re, s) {
-        if (re.exec(s) === null) return;
-        yield re.lastIndex - 1;
-        yield* matchEnds(re, s);
+    const matchAll = function* (re, s, m = re.exec(s)) { // overlapping matches
+        if (m === null) return;
+        yield m[0];
+        re.lastIndex -= m[0].length - 1;
+        yield* matchAll(re, s);
     };
     const input = document.body.textContent.trim().split('\n'),
         abbas = s => s.match(/(.)((?!\1).)\2\1/g) || [],
-        abas = s => [...matchEnds(/(.)(?!\1)(?=.\1)/g, s)].map(i => s[i] + s[i + 1]),
+        abas = s => [...matchAll(/(.)(?!\1).(?=\1)/g, s)],
         hasTLS = ip => abbas(supernet(ip)).length > 0 && abbas(hypernet(ip)).length === 0,
         hasSSL = ip => abas(supernet(ip)).some(s => abas(hypernet(ip)).includes(s[1] + s[0]));
 
