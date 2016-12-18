d0e26e4bb9feb0c4d9f0640df5840cc62c41241c
monk-time <monk-time@users.noreply.github.com>
2016-12-18 08:27:23 +0300

    Day 18

---
+++ 2016/aoc18.js
@@ -0,0 +1,22 @@
+'use strict';
+
+{
+    const nextRow = row => row.map((_, i) => {
+        const left  = i > 0              && row[i - 1];
+        const right = i < row.length - 1 && row[i + 1];
+        return left !== right;
+    });
+    const safe = row => row.reduce((n, tile) => n + (tile ? 0 : 1), 0);
+    const safeRows = (row, size) => {
+        let n = safe(row);
+        while (--size) {
+            row = nextRow(row);
+            n += safe(row);            
+        }
+        return n;
+    };
+
+    const row = [...document.body.textContent.trim()].map(ch => ch === '^'); // true === traps
+    console.log(...[40, 400000].map(n => safeRows(row, n)));
+}
+
