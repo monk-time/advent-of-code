62ba5f03b287c5488a116231566a3c9728bc2178
monk-time <monk-time@users.noreply.github.com>
2017-12-02 08:40:04 +0300

    Day 02

---
+++ 2017/aoc02.js
@@ -0,0 +1,11 @@
+'use strict';
+
+{
+    const input = document.body.textContent.trim().split('\n');
+    const rows = input.map(r => r.split('\t').map(Number).sort((x, y) => x - y));
+    const diff = r => r[r.length - 1] - r[0];
+    const quot = r => (([x, y]) => y / x)(r.filter(x => r.some(evenDiv(x))));
+    const evenDiv = x => y => x !== y && (y % x === 0 || x % y === 0);
+
+    console.log([diff, quot].map(f => rows.reduce((sum, r) => sum + f(r), 0)));
+}
