e158852c085a75f83eb0a265bf51ec3f026ca6ab
monk-time <monk-time@users.noreply.github.com>
2017-12-04 08:19:36 +0300

    Day 04

---
+++ 2017/aoc04.js
@@ -0,0 +1,11 @@
+'use strict';
+
+{
+    const phrases = document.body.textContent.trim().split('\n');
+    const noRepeats = (w, _, ws) => ws.filter(v => v === w).length === 1;
+    const sortLetters = w => [...w].sort().join('');
+    const isValid = f => ph => ph.split(' ').map(f).every(noRepeats);
+    const count = f => phrases.filter(isValid(f)).length;
+
+    console.log([w => w, sortLetters].map(count));
+}
