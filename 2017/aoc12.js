acb2928924e7c0d2819e15a40f73c08b16731ecc
monk-time <monk-time@users.noreply.github.com>
2017-12-12 09:16:30 +0300

    Day 12

---
+++ 2017/aoc12.js
@@ -0,0 +1,33 @@
+'use strict';
+
+{
+    const findGroupWith = (head, pipes) => {
+        const [queue, group] = [[head], new Set([head])];
+        while (queue.length) {
+            pipes[queue.pop()].forEach(p => {
+                if (group.has(p)) return;
+                group.add(p);
+                queue.push(p);
+            });
+        }
+
+        return group;
+    };
+
+    const allGroups = pipes => {
+        const groups = [];
+        let pipesToCheck = [...pipes.keys()].reverse();
+        while (pipesToCheck.length) {
+            const group = findGroupWith(pipesToCheck.pop(), pipes);
+            groups.push(group);
+            pipesToCheck = pipesToCheck.filter(p => !group.has(p));
+        }
+
+        return groups;
+    };
+
+    const input = document.body.textContent.trim();
+    const pipes = input.split('\n').map(s => s.match(/\d+/g).map(Number).slice(1));
+    const groups = allGroups(pipes);
+    console.log(groups[0].size, groups.length);
+}
