b69d7cb8ad92f884b775d8fe6f7b390a400f2f36
monk-time <monk-time@users.noreply.github.com>
2017-12-13 09:26:33 +0300

    Day 13: Optimize part 2

--- 2017/aoc13.js
+++ 2017/aoc13.js
@@ -2,12 +2,12 @@
 
 {
     const input = document.body.textContent.trim();
-    const scanners = input.split('\n').map(s => s.match(/\d+/g).map(Number));
-    const walk = delay => scanners
-        .filter(([d, r]) => (delay + d) % (2 * (r - 1)) === 0)
-        .reduce((severity, [d, r]) => severity + (delay + d) * r, 0);
+    const guards = input.split('\n').map(s => s.match(/\d+/g).map(Number));
+    const caughtByGuard = delay => ([d, r]) => (delay + d) % (2 * (r - 1)) === 0;
+    const severity = delay => guards.filter(caughtByGuard(delay))
+        .reduce((n, [d, r]) => n + (delay + d) * r, 0);
 
     let delay = -1;
-    while (walk(++delay) !== 0);
-    console.log([walk(0), delay]); // 6 sec
+    while (guards.some(caughtByGuard(++delay)));
+    console.log([severity(0), delay]);
 }
