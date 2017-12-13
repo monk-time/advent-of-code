26828a49e26398ec0c760e3589cdd548cc77ddcb
monk-time <monk-time@users.noreply.github.com>
2017-12-13 09:41:05 +0300

    Day 10: Fix severity for non-zero delays

--- 2017/aoc13.js
+++ 2017/aoc13.js
@@ -5,7 +5,7 @@
     const guards = input.split('\n').map(s => s.match(/\d+/g).map(Number));
     const caughtByGuard = delay => ([d, r]) => (delay + d) % (2 * (r - 1)) === 0;
     const severity = delay => guards.filter(caughtByGuard(delay))
-        .reduce((n, [d, r]) => n + (delay + d) * r, 0);
+        .reduce((n, [d, r]) => n + d * r, 0);
 
     let delay = -1;
     while (guards.some(caughtByGuard(++delay)));
