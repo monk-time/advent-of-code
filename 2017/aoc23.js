9a696b945a1a465e0b3c9f7cadd5ce75ca0cc57a
monk-time <monk-time@users.noreply.github.com>
2017-12-23 10:46:19 +0300

    Day 23: Shorten

--- 2017/aoc23.js
+++ 2017/aoc23.js
@@ -3,18 +3,16 @@
 {
     const part1 = x => (x - 2) ** 2;
     const part2 = x => {
-        let divisors = 0;
-        let step = 1;
-        while (step++ <= 1001) {
+        let nonprimes = 0;
+        for (let n = x; n <= x + 17000; n += 17) {
             let d = 2;
-            while (x % d !== 0) d++;
-            if (x !== d) divisors++;
-            x += 17;
+            while (n % d !== 0) d++;
+            if (n !== d) nonprimes++;
         }
 
-        return divisors;
+        return nonprimes;
     };
 
-    console.log(part1(65)); // the first number in the input
-    console.log(part2(65 * 100 + 100000));
+    const input = +document.body.textContent.match(/\d+/)[0]; // the first number
+    console.log(part1(input), part2(input * 100 + 100000));
 }
