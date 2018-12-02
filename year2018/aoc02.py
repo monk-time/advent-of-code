4941be70058d1ae29e303cc7cac345363dbf5c0c
monk-time <monk-time@users.noreply.github.com>
2018-12-02 16:58:33 +0300

    Day 02

---
+++ year2018/aoc02.py
@@ -0,0 +1,34 @@
+from collections import Counter
+from itertools import combinations
+from typing import Iterable
+
+from helpers import read_puzzle
+
+
+def has_exactly_n_of_any_letter(box_id: str, n: int) -> bool:
+    return n in Counter(box_id).values()
+
+
+def checksum(box_ids: Iterable[str]) -> int:
+    twice = sum(has_exactly_n_of_any_letter(box_id, 2) for box_id in box_ids)
+    thrice = sum(has_exactly_n_of_any_letter(box_id, 3) for box_id in box_ids)
+    return twice * thrice
+
+
+def matching_letters(box_a: str, box_b: str) -> str:
+    return ''.join(a for (a, b) in zip(box_a, box_b) if a == b)
+
+
+def is_correct_pair(box_a: str, box_b: str) -> bool:
+    return len(matching_letters(box_a, box_b)) == len(box_a) - 1
+
+
+def part2(box_ids: Iterable[str]) -> str:
+    matching_pair = next(t for t in combinations(box_ids, 2) if is_correct_pair(*t))
+    return matching_letters(*matching_pair)
+
+
+if __name__ == '__main__':
+    puzzle = read_puzzle().split()
+    print(checksum(puzzle))
+    print(part2(puzzle))
---
+++ year2018/tests/test_aoc02.py
@@ -0,0 +1,44 @@
+import pytest
+
+from aoc02 import checksum, has_exactly_n_of_any_letter, is_correct_pair, \
+    matching_letters, part2
+from helpers import read_puzzle
+
+box_ids = ['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab']
+bools2 = [False, True, True, False, True, True, False]
+bools3 = [False, True, False, True, False, False, True]
+
+
+@pytest.mark.parametrize("box_id, expected", zip(box_ids, bools2))
+def test_has_exactly_n_of_any_letter_basic_2(box_id, expected):
+    assert has_exactly_n_of_any_letter(box_id, 2) == expected
+
+
+@pytest.mark.parametrize("box_id, expected", zip(box_ids, bools3))
+def test_has_exactly_n_of_any_letter_basic_3(box_id, expected):
+    assert has_exactly_n_of_any_letter(box_id, 3) == expected
+
+
+def test_checksum():
+    assert checksum(box_ids) == 12
+
+
+def test_matching_letters():
+    assert matching_letters('abcde', 'axcye') == 'ace'
+    assert matching_letters('fghij', 'fguij') == 'fgij'
+
+
+def test_is_correct_pair():
+    assert not is_correct_pair('abcde', 'axcye')
+    assert is_correct_pair('fghij', 'fguij')
+
+
+def test_part2():
+    box_ids2 = ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz']
+    assert part2(box_ids2) == 'fgij'
+
+
+def test_full_puzzle():
+    puzzle = read_puzzle().split()
+    assert checksum(puzzle) == 5390
+    assert part2(puzzle) == 'nvosmkcdtdbfhyxsphzgraljq'
