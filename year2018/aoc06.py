0ac8dd143191f3b67fbd8be2f25e9a3bf86e968c
monk-time <monk-time@users.noreply.github.com>
2018-12-06 21:31:55 +0300

    Day 06: Finish the second part

--- year2018/aoc06.py
+++ year2018/aoc06.py
@@ -14,31 +14,43 @@ def parse_coords(s: str) -> List[Point]:
     return res
 
 
+def get_ranges(places) -> Tuple[range, range]:
+    p_xs = [x for x, _ in places]
+    p_ys = [y for _, y in places]
+    xs = range(min(p_xs), max(p_xs) + 1)
+    ys = range(min(p_ys), max(p_ys) + 1)
+    return xs, ys
+
+
 def closest(places: Iterable[Point], p: Point) -> Union[Point, str]:
     dists = (abs(p[0] - x) + abs(p[1] - y) for x, y in places)
     (d1, p1), (d2, p2), *_ = sorted(zip(dists, places))
     return p1 if d1 < d2 else '.'
 
 
-def fill_grid(places: Iterable[Point]) -> Tuple[Grid, range, range]:
-    p_xs = [x for x, _ in places]
-    p_ys = [y for _, y in places]
-    xs = range(min(p_xs), max(p_xs) + 1)
-    ys = range(min(p_ys), max(p_ys) + 1)
-    return dict((p, closest(places, p)) for p in product(xs, ys)), xs, ys
+def fill_grid(places: Iterable[Point]) -> Grid:
+    xs, ys = get_ranges(places)
+    return dict((p, closest(places, p)) for p in product(xs, ys))
 
 
-def count_finite(places: Iterable[Point]):
-    grid, xs, ys = fill_grid(places)
+def count_finite(places: Iterable[Point]) -> int:
+    grid = fill_grid(places)
+    xs, ys = get_ranges(places)
     edge = (set(grid[x, ys[0]] for x in xs) |
-            set(grid[x, ys[-1]] for x in xs) |
             set(grid[xs[0], y] for y in ys) |
+            set(grid[x, ys[-1]] for x in xs) |
             set(grid[xs[-1], y] for y in ys))
     return Counter(p for p in grid.values() if p not in edge).most_common(1)[0][1]
 
 
+def safe_region(places: Iterable[Point], max_sum: int) -> int:
+    xs, ys = get_ranges(places)
+    return sum(sum(abs(x - px) + abs(y - py) for px, py in places) < max_sum
+               for x in xs for y in ys)
+
+
 if __name__ == '__main__':
-    # puzzle = '1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9'
     puzzle = read_puzzle()
     places_ = parse_coords(puzzle)
     print(count_finite(places_))
+    print(safe_region(places_, 10000))
--- year2018/tests/test_aoc06.py
+++ year2018/tests/test_aoc06.py
@@ -1,7 +1,7 @@
 from string import ascii_letters
 from typing import Iterable
 
-from aoc06 import Grid, Point, count_finite, fill_grid, parse_coords
+from aoc06 import Grid, Point, count_finite, fill_grid, get_ranges, parse_coords, safe_region
 from helpers import read_puzzle
 
 sample = '1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9'
@@ -12,19 +12,22 @@ def test_parse_coords():
     assert places_ == [(1, 1), (1, 6), (8, 3), (3, 4), (5, 5), (8, 9)]
 
 
-def grid_to_str(grid: Grid, places: Iterable[Point], xs: range, ys: range) -> str:
+def test_get_ranges():
+    assert get_ranges(places_) == (range(1, 9), range(1, 10))
+
+
+def grid_to_str(grid: Grid, places: Iterable[Point]) -> str:
     names = dict(zip(places, ascii_letters))  # 52 points max
     names['.'] = '.'
     names[' '] = ' '
+    xs, ys = get_ranges(places)
     return '\n'.join(''.join(names[grid.get((i, j), ' ')] for i in xs)
                      for j in ys)
 
 
 def test_fill_grid():
-    grid, xs, ys = fill_grid(places_)
-    print()
-    print(grid_to_str(grid, places_, xs, ys))
-    assert grid_to_str(grid, places_, xs, ys) == '\n'.join([
+    grid = fill_grid(places_)
+    assert grid_to_str(grid, places_) == '\n'.join([
         'aaaa.ccc',
         'aaddeccc',
         'adddeccc',
@@ -41,7 +44,11 @@ def test_count_finite():
     assert count_finite(places_) == 17
 
 
+def test_safe_region():
+    assert safe_region(places_, 32) == 16
+
+
 def test_full_puzzle():
-    puzzle = read_puzzle()
-    places = parse_coords(puzzle)
+    places = parse_coords(read_puzzle())
     assert count_finite(places) == 5626
+    assert safe_region(places, 10000) == 46554
