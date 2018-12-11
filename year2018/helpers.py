212b7bc3a0b0031a4baf8c03ea4d820b15d0c4a8
monk-time <monk-time@users.noreply.github.com>
2018-12-11 22:35:36 +0300

    Allow loading a puzzle by its number (e.g. in console)

--- year2018/helpers.py
+++ year2018/helpers.py
@@ -1,16 +1,22 @@
 import inspect
 import re
 from pathlib import Path
+from typing import Optional
 
 
-def read_puzzle() -> str:
+def read_puzzle(number: Optional[int] = None) -> str:
     """Read the contents of the input file for the current puzzle."""
     # The number of the puzzle is determined by the filename of the caller
-    caller: str = Path(inspect.stack()[1].filename).stem.replace('test_', '')
-    pzl_num = re.search(r'\d+', caller)
-    if not pzl_num:
-        raise FileNotFoundError('Cannot find the relevant puzzle input file. '
-                                'Make sure this function is called from a file '
-                                'with a puzzle number in the filename.')
-    puzzle_input = Path(__file__).parent / 'inputs' / f'aoc{pzl_num.group()}.txt'
+    if number:
+        year_dir = Path('year2018')
+    else:
+        caller: str = Path(inspect.stack()[1].filename).stem.replace('test_', '')
+        match = re.search(r'\d+', caller)
+        if not match:
+            raise FileNotFoundError('Cannot find the relevant puzzle input file. '
+                                    'Make sure this function is called from a file '
+                                    'with a puzzle number in the filename.')
+        number = int(match.group())
+        year_dir = Path(__file__).parent
+    puzzle_input = year_dir / 'inputs' / f'aoc{number:02d}.txt'
     return puzzle_input.read_text(encoding='utf-8').strip()
