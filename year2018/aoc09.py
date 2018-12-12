2015440318e1aa8462016a0bc725c872cbbf9896
monk-time <monk-time@users.noreply.github.com>
2018-12-12 19:20:09 +0300

    Day 09: Use a 3-length list instead of a dataclass (5.4 sec, 796 MB)

--- year2018/aoc09.py
+++ year2018/aoc09.py
@@ -1,32 +1,25 @@
 import re
-from dataclasses import dataclass, field
 
 from helpers import read_puzzle, timed
-
-
-@dataclass
-class Marble:
-    value: int
-    prev: 'Marble' = field(default=None, repr=False)
-    next: 'Marble' = field(default=None, repr=False)
 
 
 @timed
 def high_score(s: str) -> int:
     players, last_marble = map(int, re.findall(r'\d+', s))
-    marble, scores = Marble(0), [0] * players
-    marble.prev, marble.next = marble, marble
+    scores = [0] * players
+    marble = [None, 0, None]
+    marble[0] = marble[2] = marble
     for value in range(1, last_marble + 1):
         if value % 23 != 0:
-            a, b = marble.next, marble.next.next
-            marble = Marble(value, prev=a, next=b)
-            a.next = b.prev = marble
+            a, b = marble[2], marble[2][2]
+            marble = [a, value, b]
+            a[2] = b[0] = marble
         else:
             for _ in range(7):
-                marble = marble.prev
-            scores[value % players] += value + marble.value
-            marble.prev.next = marble.next
-            marble = marble.next
+                marble = marble[0]
+            scores[value % players] += value + marble[1]
+            marble[0][2] = marble[2]
+            marble = marble[2]
     return max(scores)
 
 
