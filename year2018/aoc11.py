07efd437ea91590aba9292a71f554426abe598df
monk-time <monk-time@users.noreply.github.com>
2018-12-13 16:45:33 +0300

    Day 11: Part 2, 150 sec parallelized with some caching

--- year2018/aoc11.py
+++ year2018/aoc11.py
@@ -1,7 +1,11 @@
+import multiprocessing
+from collections import deque
 from functools import lru_cache
 from itertools import product
 
 from helpers import read_puzzle, timed
+
+GRID = 300
 
 
 @lru_cache(maxsize=None)
@@ -9,18 +13,49 @@ def power_level(serial: int, x: int, y: int) -> int:
     return ((x + 10) * y + serial) * (x + 10) // 100 % 10 - 5
 
 
-def total_power(serial: int, x: int, y: int) -> int:
+def total_power(serial: int, x: int, y: int, size: int) -> int:
     return sum(power_level(serial, x + dx, y + dy)
-               for dx, dy in product(range(3), repeat=2))
+               for dx, dy in product(range(size), repeat=2))
 
 
 @timed
-def max_square(serial: int):
-    key = lambda coords: total_power(serial, *coords)
-    x, y = max(product(range(1, 299), repeat=2), key=key)
-    return f'{x},{y}'
+def max_square_naive(serial: int, size: int = 3):
+    """Find the maximum power by recalculating every square afresh."""
+    corners = product(range(1, GRID + 2 - size), repeat=2)
+    return max((total_power(serial, *c, size), *c, size) for c in corners)
+
+
+def max_square_by_columns(serial: int, size: int = 3):
+    """Find the maximum power by moving a square in a LTR-then-TTB direction.
+
+    On each horizontal pass a square is cached and moved column by column."""
+    column = lambda i, j: sum(power_level(serial, i, j + d) for d in range(size))
+    max_power, max_coords = float('-inf'), (1, 1)
+    for y in range(1, GRID + 2 - size):
+        # A square is represented by a list of totals of its columns
+        square = deque(column(1 + d, y) for d in range(size))
+        power = sum(square)
+        if power > max_power:
+            max_power, max_coords = power, (1, y)
+        for x in range(size + 1, GRID + 1):
+            # Add a new column and check if the total has increased
+            c_old = square.popleft()
+            c_new = column(x, y)
+            square.append(c_new)
+            power += c_new - c_old
+            if power > max_power:
+                max_power, max_coords = power, (x - size + 1, y)
+    return (max_power, *max_coords, size)
+
+
+@timed
+def max_square_any_size(serial: int):
+    with multiprocessing.Pool(8) as p:
+        res = p.starmap(max_square_by_columns, ((serial, size) for size in range(1, GRID + 1)))
+    return max(res)
 
 
 if __name__ == '__main__':
     serial_ = int(read_puzzle())
-    print(max_square(serial_))
+    print(max_square_naive(serial_))
+    print(max_square_any_size(serial_))
--- year2018/tests/test_aoc11.py
+++ year2018/tests/test_aoc11.py
@@ -1,5 +1,12 @@
-from aoc11 import max_square, power_level
+from typing import List
+
+from aoc11 import max_square_any_size, max_square_by_columns, max_square_naive, power_level
 from helpers import read_puzzle
+
+
+def segment(serial: int, x: int, y: int, size: int) -> List[str]:
+    return [' '.join(f'{power_level(serial, x + dx, y + dy):4d}' for dx in range(size))
+            for dy in range(size)]
 
 
 def test_power_level():
@@ -9,11 +16,17 @@ def test_power_level():
     assert power_level(serial=71, x=101, y=153) == 4
 
 
-def test_max_square():
-    assert max_square(18) == '33,45'
-    assert max_square(42) == '21,61'
+def test_max_square_naive():
+    assert max_square_naive(18) == (29, 33, 45, 3)
+    assert max_square_naive(42) == (30, 21, 61, 3)
+
+
+def test_max_square_by_columns():
+    assert max_square_by_columns(18) == (29, 33, 45, 3)
+    assert max_square_by_columns(42) == (30, 21, 61, 3)
 
 
 def test_full_puzzle():
     serial = int(read_puzzle())
-    assert max_square(serial) == '20,46'
+    assert max_square_naive(serial) == (30, 20, 46, 3)
+    assert max_square_any_size(serial) == (158, 231, 65, 14)
