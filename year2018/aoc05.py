1a8bf59bc2796f8bdef0a76ef5fdec716fa0c837
monk-time <monk-time@users.noreply.github.com>
2019-10-28 21:09:17 +0300

    Day 05: Small fixes

--- year2018/aoc05.py
+++ year2018/aoc05.py
@@ -1,5 +1,3 @@
-from typing import Iterable
-
 from helpers import read_puzzle
 
 
@@ -7,9 +5,9 @@ def are_matching(a: str, b: str) -> bool:
     return a != b and (a.lower() == b or b.lower() == a)
 
 
-def react(poly: Iterable[str], skip: str = None) -> str:
+def react(poly: str, skip: str = None) -> str:
     new_poly = ''
-    for unit in iter(poly):
+    for unit in poly:
         if unit.lower() == skip:
             continue
         if new_poly and are_matching(unit, new_poly[-1]):
@@ -25,7 +23,7 @@ def improve(poly: str) -> int:
 
 
 def solve():
-    poly = react(read_puzzle())
+    poly = react(read_puzzle())  # fully react the input before doing part 2
     return len(poly), improve(poly)
 
 
