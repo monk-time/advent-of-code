1714109bbcaf312a470eb26af61b7ce2d87850f3
monk-time <monk-time@users.noreply.github.com>
2019-10-28 15:36:33 +0300

    Day 03: Rewrite with Counter instead of overlapping

--- year2018/aoc03.py
+++ year2018/aoc03.py
@@ -1,7 +1,7 @@
 import re
-from collections import namedtuple
-from itertools import chain, combinations, product
-from typing import Iterable, List, Optional, Tuple
+from collections import Counter, namedtuple
+from itertools import product
+from typing import Iterable, List, Tuple
 
 from helpers import read_puzzle
 
@@ -12,34 +12,15 @@ def parse(claim: str) -> Claim:
     return Claim(*map(int, re.findall(r'\d+', claim)))
 
 
-def overlap(a: Claim, b: Claim) -> Optional[Claim]:
-    left, right = max(a.left, b.left), min(a.left + a.width, b.left + b.width)
-    top, bottom = max(a.top, b.top), min(a.top + a.height, b.top + b.height)
-    if left >= right or top >= bottom:
-        return None
-    return Claim((a.id, b.id), left, top, right - left, bottom - top)
-
-
-def claim_to_points(a: Claim) -> Iterable[Tuple[int, int]]:
+def squares(a: Claim) -> Iterable[Tuple[int, int]]:
     return product(range(a.left, a.left + a.width), range(a.top, a.top + a.height))
 
 
-def overlap_all(claims: List[Claim]) -> Iterable[Claim]:
-    for a, b in combinations(claims, 2):
-        o = overlap(a, b)
-        if o:
-            yield o
-
-
 def solve(claims: List[Claim]):
-    overlaps = list(overlap_all(claims))
-    points = map(claim_to_points, overlaps)
-    inches = len(set(chain.from_iterable(points)))
-
-    ids = [c.id for c in claims]
-    used_ids = set(chain.from_iterable(c.id for c in overlaps))
-    unused = next(id_ for id_ in ids if id_ not in used_ids)
-    return inches, unused
+    layers = Counter(sq for c in claims for sq in squares(c))
+    inches = sum(1 for v in layers.values() if v > 1)
+    unused_id = next(c.id for c in claims if all(layers[sq] == 1 for sq in squares(c)))
+    return inches, unused_id
 
 
 if __name__ == '__main__':
--- year2018/tests/test_aoc03.py
+++ year2018/tests/test_aoc03.py
@@ -1,4 +1,4 @@
-from aoc03 import Claim, claim_to_points, solve, parse, overlap
+from aoc03 import Claim, parse, solve, squares
 from helpers import read_puzzle
 
 a = parse('#1 @ 1,3: 4x4')
@@ -12,15 +12,8 @@ def test_parse():
     assert parse('#6 @ 409,863: 17x10') == Claim(6, 409, 863, 17, 10)
 
 
-def test_overlap():
-    assert overlap(a, b) == Claim((1, 2), 3, 3, 2, 2)
-    assert overlap(b, a) == Claim((2, 1), 3, 3, 2, 2)
-    assert overlap(a, c) is None
-    assert overlap(b, c) is None
-
-
-def test_claim_to_points():
-    assert list(claim_to_points(c)) == [(5, 5), (5, 6), (6, 5), (6, 6)]
+def test_squares():
+    assert list(squares(c)) == [(5, 5), (5, 6), (6, 5), (6, 6)]
 
 
 def test_solve():
