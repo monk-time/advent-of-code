5159c182f624de9e8f714896c5478ab6d2a50b1b
monk-time <monk-time@users.noreply.github.com>
2019-10-29 15:41:45 +0300

    Day 10: Add tests

--- year2018/aoc10.py
+++ year2018/aoc10.py
@@ -1,41 +1,42 @@
 import re
-from dataclasses import dataclass
-from typing import Iterable, List, Tuple
+from collections import namedtuple
+from typing import List, Tuple
 
 from helpers import read_puzzle
 
-
-@dataclass
-class Point:
-    pos: Tuple[int, int]
-    delta: Tuple[int, int]
-
-
+Point = namedtuple('Point', 'x y dx dy')
 Points = List[Point]
 
 
-def parse(s: str) -> Iterable[Point]:
-    for line in s.splitlines():
-        x, y, dx, dy = map(int, re.findall(r'-?\d+', line))
-        yield Point((x, y), (dx, dy))
-
-
-def move_all(points: Points) -> Points:
-    return [Point((p.pos[0] + p.delta[0], p.pos[1] + p.delta[1]), p.delta)
-            for p in points]
+def parse(s: str) -> Points:
+    return [Point(*map(int, re.findall(r'-?\d+', l))) for l in s.splitlines()]
 
 
 def dimensions(points: Points) -> Tuple[int, int, int, int]:
-    min_x = min(p.pos[0] for p in points)
-    max_x = max(p.pos[0] for p in points)
-    min_y = min(p.pos[1] for p in points)
-    max_y = max(p.pos[1] for p in points)
+    min_x = min(p.x for p in points)
+    max_x = max(p.x for p in points)
+    min_y = min(p.y for p in points)
+    max_y = max(p.y for p in points)
     w = max_x - min_x + 1
     h = max_y - min_y + 1
     return w, h, min_x, min_y
 
 
-def solve(points: Points):
+def points_to_str(points: Points) -> str:
+    w, h, min_x, min_y = dimensions(points)
+    screen = ['.' * w] * h
+    for p in points:
+        x, y = p.x - min_x, p.y - min_y
+        s = screen[y]
+        screen[y] = s[:x] + '#' + s[x + 1:]
+    return '\n'.join(screen)
+
+
+def move_all(points: Points) -> Points:
+    return [Point(p.x + p.dx, p.y + p.dy, p.dx, p.dy) for p in points]
+
+
+def move_while_shrinks(points: Points):
     dims, sec = dimensions(points), 0
     while True:
         dims_old, points_old = dims, points
@@ -44,16 +45,12 @@ def solve(points: Points):
         if dims_old[0] < dims[0] or dims_old[1] < dims[1]:
             break
         sec += 1
-    points = points_old
-    w, h, min_x, min_y = dims_old
-    screen = ['.' * w] * h
-    for p in points:
-        x, y = p.pos[0] - min_x, p.pos[1] - min_y
-        s = screen[y]
-        screen[y] = s[:x] + '#' + s[x + 1:]
-    return '\n'.join(screen), sec
+    return points_to_str(points_old), sec
+
+
+def solve():
+    return move_while_shrinks(parse(read_puzzle()))
 
 
 if __name__ == '__main__':
-    points_ = list(parse(read_puzzle()))
-    print(*solve(points_))
+    print(*solve())
