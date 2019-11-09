a899b9711d9bf3996e1b550dcedf6a43fd02c190
monk-time <monk-time@users.noreply.github.com>
2019-11-09 20:14:57 +0300

    Day 15: Part 1 (7 sec)

--- year2018/aoc15.py
+++ year2018/aoc15.py
@@ -1,65 +1,12 @@
+from collections import deque
 from dataclasses import dataclass, replace
 from itertools import chain
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 
-from helpers import block_unwrap, read_puzzle
+from helpers import read_puzzle
 
 Square = Tuple[int, int]
 Squares = Dict[Square, str]
-
-
-# TODO: remove or integrate into __str__
-def replace_chars(s: str, chars: Squares):
-    """Replace characters in a multiline string by a coords-to-char mapping."""
-    lines = s.splitlines()
-    for sq, ch in chars.items():
-        i, j = sq
-        lines[i] = lines[i][:j] + ch + lines[i][j + 1:]
-    return '\n'.join(lines)
-
-
-@dataclass
-class State:
-    height: int
-    width: int
-    map: Squares
-    units: List['Unit']
-    rounds_played: int = 0
-    finished: bool = False
-
-    @classmethod
-    def fromstring(cls, s: str):
-        lines = s.splitlines()
-        map_, units = {}, []
-        for i, line in enumerate(lines):
-            for j, ch in enumerate(line):
-                map_[(i, j)] = ch
-                if ch in 'EG':
-                    units.append(Unit((i, j), ch))
-        return cls(height=len(lines), width=len(lines[0]),
-                   map=map_, units=units)
-
-    def __str__(self, hp: bool = False):
-        """Get a text representation of the map."""
-        map_ = [''.join(self.map[(i, j)] for j in range(self.width))
-                for i in range(self.height)]
-        if hp:
-            for unit in self.units:
-                i, j = unit.sq
-                map_[i] += ', ' if len(map_[i]) > self.width else '   '
-                map_[i] += f'{unit.type}({unit.hp})'
-        return '\n'.join(map_)
-
-    def deepcopy(self):
-        """Return a deep copy of the state."""
-        return replace(self, map=self.map.copy(),
-                       units=[replace(u) for u in self.units])
-
-    def squares_in_range(self, sq: Square) -> List[Square]:
-        """Get all open (.) squares adjacent to the square."""
-        i, j = sq
-        return [sq2 for sq2 in [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
-                if self.map.get(sq2, None) == '.']
 
 
 @dataclass(order=True)
@@ -82,6 +29,97 @@ class Unit:
                 (sj == tj and abs(si - ti) == 1))
 
 
+@dataclass
+class State:
+    height: int
+    width: int
+    map: Squares
+    units: List[Unit]
+    rounds_played: int = 0
+    finished: bool = False
+
+    @classmethod
+    def fromstring(cls, s: str):
+        lines = s.splitlines()
+        map_, units = {}, []
+        for i, line in enumerate(lines):
+            for j, ch in enumerate(line):
+                map_[(i, j)] = ch
+                if ch in 'EG':
+                    units.append(Unit((i, j), ch))
+        return cls(height=len(lines), width=len(lines[0]),
+                   map=map_, units=units)
+
+    def __str__(self, hp: bool = False):
+        """Get a text representation of the map."""
+        map_ = [''.join(self.map[(i, j)] for j in range(self.width))
+                for i in range(self.height)]
+        if hp:
+            for unit in sorted(self.units):
+                i, j = unit.sq
+                map_[i] += ', ' if len(map_[i]) > self.width else '   '
+                map_[i] += f'{unit.type}({unit.hp})'
+        return '\n'.join(map_)
+
+    def deepcopy(self) -> 'State':
+        """Return a deep copy of the state."""
+        return replace(self, map=self.map.copy(),
+                       units=[replace(u) for u in self.units])
+
+    def squares_in_range(self, sq: Square) -> List[Square]:
+        """Get all open (.) squares adjacent to the square."""
+        i, j = sq
+        return [sq2 for sq2 in [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
+                if self.map.get(sq2, None) == '.']
+
+    def find_path(self, unit: Unit, sq_trg: Square) -> \
+            Optional[Tuple[int, Square, Square]]:
+        """Find a square along the shortest path from the unit
+        to the target square and the path's length."""
+        # Temporarily hide the source unit so that BFS can work
+        self.map[unit.sq] = '.'
+        # Breadth-first search (starting from the end)
+        dist = {sq_trg: 0}
+        queue = deque([sq_trg])
+        while queue:
+            sq = queue.popleft()
+            if sq == unit.sq:
+                break
+            for sq_next in self.squares_in_range(sq):
+                if sq_next not in dist:
+                    dist[sq_next] = dist[sq] + 1
+                    queue.append(sq_next)
+        else:  # no break
+            self.map[unit.sq] = unit.type
+            return None
+        # If there are multiple steps from the start along the shortest path
+        # available, choose in reading order.
+        sqs = sorted((dist[sq], sq_trg, sq)
+                     for sq in self.squares_in_range(unit.sq)
+                     if sq in dist)
+        self.map[unit.sq] = unit.type
+        return sqs[0]
+
+    def move(self, unit, targets: List[Unit]):
+        sqs = set(chain(*(self.squares_in_range(t.sq) for t in targets)))
+        reachable = sorted(p for sq in sqs
+                           if (p := self.find_path(unit, sq)) is not None)
+        if not reachable:
+            return
+        self.map[unit.sq] = '.'
+        unit.sq = reachable[0][2]
+        self.map[unit.sq] = unit.type
+
+    def hit(self, target: Unit, dmg: int):
+        target.hp -= min(dmg, target.hp)
+        if target.hp <= 0:
+            self.units.remove(target)
+            self.map[target.sq] = '.'
+
+    def hash(self) -> int:
+        return self.rounds_played * sum(u.hp for u in self.units)
+
+
 def play_round(st: State) -> State:
     """Simulate one round of the game without mutating the state."""
     st = st.deepcopy()
@@ -98,47 +136,32 @@ def play_round(st: State) -> State:
 
         targets_in_range = list(filter(unit.is_in_range, targets))
         if not targets_in_range:
-            # TODO: move
-            sqs = set(chain(*(st.squares_in_range(t.sq) for t in targets)))
-            # if none available, finish
-            # find the closest open square (if several, choose in RO)
-            # move
-            pass
+            st.move(unit, targets)
 
         # Check adjacent targets again after moving
         targets_in_range = list(filter(unit.is_in_range, targets))
         if not targets_in_range:
             continue
+
         # Target a unit with the lowest hp in reading order
         targets_in_range.sort(key=lambda t: (t.hp, t.sq))
-        target = targets_in_range[0]
-        target.hp -= unit.power
-        if target.hp <= 0:
-            st.units.remove(target)
-            st.map[target.sq] = '.'
+        st.hit(targets_in_range[0], dmg=unit.power)
     else:  # no break, i.e. the combat has not ended
         st.rounds_played += 1
 
     return st
 
 
+def outcome(s: str) -> int:
+    st = State.fromstring(s)
+    while not st.finished:
+        st = play_round(st)
+    return st.hash()
+
+
 def solve():
-    return State.fromstring(read_puzzle())
+    return outcome(read_puzzle())
 
 
 if __name__ == '__main__':
-    # print(solve())
-    # st_ = State(read_puzzle())
-    st_ = State.fromstring(block_unwrap("""
-        #######
-        #E..G.#
-        #...#.#
-        #.G.#G#
-        #######
-    """, border=False))
-    print('\n' * 20)
-    while True:
-        print(f'After {st_.rounds_played} rounds:')
-        print(st_.__str__(hp=True))
-        input()
-        st_ = play_round(st_)
+    print(solve())
--- year2018/tests/test_aoc15.py
+++ year2018/tests/test_aoc15.py
@@ -1,7 +1,35 @@
 import pytest
 
-from aoc15 import State, Unit, play_round, replace_chars
+from aoc15 import State, Unit, outcome, play_round, solve
 from helpers import block_unwrap
+
+
+def test_unit_is_enemy():
+    g = Unit((1, 2), 'G')
+    e1 = Unit((2, 2), 'E')
+    e2 = Unit((2, 2), 'E')
+    assert g.is_enemy(e1)
+    assert not e1.is_enemy(e2)
+
+
+def test_unit_is_in_range():
+    """
+        012
+        345
+        678
+    """
+    us = State.fromstring('EEE\nEEE\nEEE').units
+    all_in_range = lambda n: [u for u in us if u.is_in_range(us[n])]
+    assert all_in_range(0) == [us[1], us[3]]
+    assert all_in_range(2) == [us[1], us[5]]
+    assert all_in_range(6) == [us[3], us[7]]
+    assert all_in_range(8) == [us[5], us[7]]
+    assert all_in_range(1) == [us[0], us[2], us[4]]
+    assert all_in_range(3) == [us[0], us[4], us[6]]
+    assert all_in_range(5) == [us[2], us[4], us[8]]
+    assert all_in_range(7) == [us[4], us[6], us[8]]
+    assert all_in_range(4) == [us[1], us[3], us[5], us[7]]
+
 
 sample = """
 #######
@@ -28,15 +56,41 @@ def st_cross():
     """, border=False))
 
 
-def test_replace_chars():
-    chars = {(1, 1): '@', (1, 3): '%', (3, 5): '*', (4, 6): '&'}
-    assert replace_chars(sample, chars) == block_unwrap("""
+@pytest.fixture
+def st_move1():
+    return State.fromstring(block_unwrap("""
         #######
-        #@G%E.#
-        #E.G.E#
-        #.G.E*#
-        ######&
-    """, border=False)
+        #E..G.#
+        #...#.#
+        #.G.#G#
+        #######
+    """, border=False))
+
+
+@pytest.fixture
+def st_move2():
+    return State.fromstring(block_unwrap("""
+        #######
+        #.E...#
+        #...?.#
+        #..?G?#
+        #######
+    """, border=False))
+
+
+@pytest.fixture
+def st_move_big():
+    return State.fromstring(block_unwrap("""
+        #########
+        #G..G..G#
+        #.......#
+        #.......#
+        #G..E..G#
+        #.......#
+        #.......#
+        #G..G..G#
+        #########
+    """, border=False))
 
 
 def test_state_fromstring(st_sample):
@@ -100,31 +154,114 @@ def test_state_squares_in_range(st_sample, st_cross):
     assert st_cross.squares_in_range((2, 2)) == []
 
 
-def test_unit_is_enemy():
-    g = Unit((1, 2), 'G')
-    e1 = Unit((2, 2), 'E')
-    e2 = Unit((2, 2), 'E')
-    assert g.is_enemy(e1)
-    assert not e1.is_enemy(e2)
+def test_state_find_path(st_move1, st_move2, st_move_big):
+    u = st_move1.units[0]
+    assert st_move1.find_path(u, (1, 3)) == (1, (1, 3), (1, 2))
+    assert st_move1.find_path(u, (2, 2)) == (1, (2, 2), (1, 2))
+    assert st_move1.find_path(u, (3, 1)) == (1, (3, 1), (2, 1))
+    assert st_move1.find_path(u, (3, 3)) == (3, (3, 3), (1, 2))
+    assert st_move1.find_path(u, (5, 1)) is None
+    assert st_move1.find_path(u, (5, 2)) is None
+    assert st_move1.map[(1, 1)] == 'E'
+
+    u = st_move1.units[2]
+    assert u.sq == (3, 2)
+    assert st_move1.find_path(u, (1, 3)) == (2, (1, 3), (2, 2))
+    assert st_move1.map[(3, 2)] == 'G'
+
+    u = st_move2.units[0]
+    assert st_move2.find_path(u, (2, 4)) == (2, (2, 4), (1, 3))
+    assert st_move2.find_path(u, (3, 3)) == (2, (3, 3), (1, 3))
+    assert st_move2.find_path(u, (3, 5)) == (4, (3, 5), (1, 3))
+
+    u = st_move_big.units[3]
+    assert st_move_big.find_path(u, (4, 3)) == (1, (4, 3), (4, 2))
 
 
-def test_unit_is_in_range():
-    """
-        012
-        345
-        678
-    """
-    us = State.fromstring('EEE\nEEE\nEEE').units
-    all_in_range = lambda n: [u for u in us if u.is_in_range(us[n])]
-    assert all_in_range(0) == [us[1], us[3]]
-    assert all_in_range(2) == [us[1], us[5]]
-    assert all_in_range(6) == [us[3], us[7]]
-    assert all_in_range(8) == [us[5], us[7]]
-    assert all_in_range(1) == [us[0], us[2], us[4]]
-    assert all_in_range(3) == [us[0], us[4], us[6]]
-    assert all_in_range(5) == [us[2], us[4], us[8]]
-    assert all_in_range(7) == [us[4], us[6], us[8]]
-    assert all_in_range(4) == [us[1], us[3], us[5], us[7]]
+def test_state_move(st_move1):
+    unit, targets = st_move1.units[0], st_move1.units[1:]
+    st_move1.move(unit, targets)
+    assert str(st_move1) == block_unwrap("""
+        #######
+        #.E.G.#
+        #...#.#
+        #.G.#G#
+        #######
+    """, border=False)
+
+
+def test_state_move_tricky():
+    st = State.fromstring(block_unwrap("""
+        #######
+        ###...#
+        ###.#.#
+        #.E.#.#
+        #.###G#
+        #.....#
+        #######
+    """, border=False))
+    e, g = st.units
+    st.move(e, [g])
+    assert str(st) == block_unwrap("""
+        #######
+        ###...#
+        ###.#.#
+        #..E#.#
+        #.###G#
+        #.....#
+        #######
+    """, border=False)
+
+
+def test_state_move_all(st_move_big):
+    st = play_round(st_move_big)
+    assert str(st) == block_unwrap("""
+        #########
+        #.G...G.#
+        #...G...#
+        #...E..G#
+        #.G.....#
+        #.......#
+        #G..G..G#
+        #.......#
+        #########
+    """, border=False)
+
+    st = play_round(st)
+    assert str(st) == block_unwrap("""
+        #########
+        #..G.G..#
+        #...G...#
+        #.G.E.G.#
+        #.......#
+        #G..G..G#
+        #.......#
+        #.......#
+        #########
+    """, border=False)
+
+    st = play_round(st)
+    assert str(st) == block_unwrap("""
+        #########
+        #.......#
+        #..GGG..#
+        #..GEG..#
+        #G..G...#
+        #......G#
+        #.......#
+        #.......#
+        #########
+    """, border=False)
+
+
+def test_state_hit(st_sample):
+    target = st_sample.units[0]
+    st_sample.hit(target, dmg=3)
+    assert target.hp == 197
+
+    st_sample.hit(target, dmg=200)
+    assert target not in st_sample.units
+    assert target.hp == 0
 
 
 def test_play_round_no_moves(st_cross):
@@ -183,3 +320,133 @@ def test_play_round_played_rounds(st_cross):
         st_cross = play_round(st_cross)
     # 200 = 16 * 4 * 3 + 8, combat ends before the last goblin can act
     assert st_cross.rounds_played == 16
+
+
+def test_play_round_full():
+    st = State.fromstring(block_unwrap("""
+        #######
+        #.G...#
+        #...EG#
+        #.#.#G#
+        #..G#E#
+        #.....#
+        #######
+    """, border=False))
+
+    st = play_round(st)
+    assert st.__str__(hp=True) == block_unwrap("""
+        #######
+        #..G..#   G(200)
+        #...EG#   E(197), G(197)
+        #.#G#G#   G(200), G(197)
+        #...#E#   E(197)
+        #.....#
+        #######
+    """, border=False)
+
+    st = play_round(st)
+    assert st.__str__(hp=True) == block_unwrap("""
+        #######
+        #...G.#   G(200)
+        #..GEG#   G(200), E(188), G(194)
+        #.#.#G#   G(194)
+        #...#E#   E(194)
+        #.....#
+        #######
+    """, border=False)
+
+    while st.rounds_played != 23:
+        st = play_round(st)
+    assert st.__str__(hp=True) == block_unwrap("""
+        #######
+        #...G.#   G(200)
+        #..G.G#   G(200), G(131)
+        #.#.#G#   G(131)
+        #...#E#   E(131)
+        #.....#
+        #######
+    """, border=False)
+
+    while st.rounds_played != 28:
+        st = play_round(st)
+    assert st.__str__(hp=True) == block_unwrap("""
+        #######
+        #G....#   G(200)
+        #.G...#   G(131)
+        #.#.#G#   G(116)
+        #...#E#   E(113)
+        #....G#   G(200)
+        #######
+    """, border=False)
+
+    while not st.finished:
+        st = play_round(st)
+    assert st.__str__(hp=True) == block_unwrap("""
+        #######
+        #G....#   G(200)
+        #.G...#   G(131)
+        #.#.#G#   G(59)
+        #...#.#
+        #....G#   G(200)
+        #######
+    """, border=False)
+    assert st.rounds_played == 47
+    assert st.hash() == 27730
+
+
+def test_outcome():
+    assert outcome(block_unwrap("""
+        #######
+        #G..#E#
+        #E#E.E#
+        #G.##.#
+        #...#E#
+        #...E.#
+        #######
+    """, border=False)) == 36334
+
+    assert outcome(block_unwrap("""
+        #######   
+        #E..EG#
+        #.#G.E#
+        #E.##E#
+        #G..#.#
+        #..E#.#   
+        #######   
+    """, border=False)) == 39514
+
+    assert outcome(block_unwrap("""
+        #######   
+        #E.G#.#
+        #.#G..#
+        #G.#.G#   
+        #G..#.#
+        #...E.#
+        #######   
+    """, border=False)) == 27755
+
+    assert outcome(block_unwrap("""
+        #######   
+        #.E...#   
+        #.#..G#
+        #.###.#   
+        #E#G#G#   
+        #...#G#
+        #######   
+    """, border=False)) == 28944
+
+    assert outcome(block_unwrap("""
+        #########   
+        #G......#
+        #.E.#...#
+        #..##..G#
+        #...##..#   
+        #...#...#
+        #.G...G.#   
+        #.....G.#   
+        #########   
+    """, border=False)) == 18740
+
+
+def test_solve():
+    assert solve() == 191575
