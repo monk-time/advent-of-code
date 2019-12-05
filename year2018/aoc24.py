8c091dc173ec1b2abbf64e0a72522cb1a520f969
monk-time <monk-time@users.noreply.github.com>
2019-12-05 11:41:38 +0300

    Day 24: Part 1

---
+++ year2018/aoc24.py
@@ -0,0 +1,108 @@
+import re
+from dataclasses import dataclass
+from typing import Dict, List, Match, Tuple
+
+from helpers import read_puzzle
+
+
+@dataclass
+class Group:
+    id_: int
+    team: str
+    units: int
+    unit_hp: int
+    weak: List[str]
+    immune: List[str]
+    dmg: int
+    dmg_type: str
+    init: int
+
+    @classmethod
+    def frommatch(cls, m: Match, **kwargs) -> 'Group':
+        d = {k: int(v) if v and v.isdigit() else v
+             for k, v in m.groupdict().items()}
+        traits = parse_traits(d['traits'])
+        del d['traits']
+        return Group(**d, **kwargs, **traits)
+
+    def __hash__(self):
+        return hash(self.name)
+
+    @property
+    def name(self) -> str:
+        return f'{self.team} group {self.id_}'
+
+    @property
+    def power(self) -> int:
+        return self.units * self.dmg
+
+    def dmg_to(self, target: 'Group') -> int:
+        if self.dmg_type in target.immune:
+            return 0
+        return self.power * (2 if self.dmg_type in target.weak else 1)
+
+
+def parse(s: str) -> Tuple[List[Group], List[Group]]:
+    regex = r"""^
+        (?P<units>\d+)\ units.+?
+        (?P<unit_hp>\d+)\ hit\ points\ 
+        (?:\((?P<traits>.+?)\))?.+?
+        (?P<dmg>\d+)\ (?P<dmg_type>\w+?)\ damage.+?
+        (?P<init>\d+)
+        $"""
+    flags = re.MULTILINE | re.VERBOSE
+    immune, infect = (
+        [Group.frommatch(m, id_=i, team=['Immune System', 'Infection'][j])
+         for i, m in enumerate(re.finditer(regex, s2, flags), start=1)]
+        for j, s2 in enumerate(s.split('\n\n')))
+    return immune, infect
+
+
+def parse_traits(s: str) -> Dict[str, List[str]]:
+    d = {'weak': [], 'immune': []}
+    if s:
+        for s2 in s.split('; '):
+            trait, dmg_types = s2.split(' to ')
+            d[trait] = dmg_types.split(', ')
+    return d
+
+
+def fight(immune: List[Group], infect: List[Group]):
+    teams = {'Immune System': immune, 'Infection': infect}
+    enemy_teams = {'Immune System': infect, 'Infection': immune}
+    targets = {}
+
+    # Target selection phase
+    for team_name, team in teams.items():
+        enemy_team = enemy_teams[team_name]
+        for g in sorted(team, key=lambda x: (-x.power, -x.init)):
+            ts = sorted((x for x in enemy_team if x not in targets.values()),
+                        key=lambda x: (-g.dmg_to(x), -x.power, -x.init))
+            if ts and g.dmg_to(ts[0]) > 0:
+                targets[g] = ts[0]
+
+    # Attacking phase
+    for g in sorted((*immune, *infect), key=lambda x: -x.init):
+        if g not in targets or g.units == 0:
+            continue
+        t = targets[g]
+        units_killed = g.dmg_to(t) // t.unit_hp
+        t.units = max(0, t.units - units_killed)
+        if t.units == 0:
+            teams[t.team].remove(t)
+    return immune, infect
+
+
+def combat(immune: List[Group], infect: List[Group]) -> int:
+    while immune and infect:
+        immune, infect = fight(immune, infect)
+    return sum(g.units for g in (*immune, *infect))
+
+
+def solve():
+    immune, infect = parse(read_puzzle())
+    return combat(immune, infect)
+
+
+if __name__ == '__main__':
+    print(solve())
---
+++ year2018/tests/test_aoc24.py
@@ -0,0 +1,65 @@
+from inspect import cleandoc
+
+import pytest
+
+from aoc24 import Group, combat, parse, parse_traits, solve
+
+sample_str = cleandoc("""
+    Immune System:
+    17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
+    989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
+    
+    Infection:
+    801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
+    4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4
+""")
+
+
+@pytest.fixture
+def sample():
+    return parse(sample_str)
+
+
+def test_parse(sample):
+    assert sample == (
+        [Group(id_=1, team='Immune System', units=17, unit_hp=5390,
+               weak=['radiation', 'bludgeoning'], immune=[],
+               dmg=4507, dmg_type='fire', init=2),
+         Group(id_=2, team='Immune System', units=989, unit_hp=1274,
+               weak=['bludgeoning', 'slashing'], immune=['fire'],
+               dmg=25, dmg_type='slashing', init=3)],
+        [Group(id_=1, team='Infection', units=801, unit_hp=4706,
+               weak=['radiation'], immune=[],
+               dmg=116, dmg_type='bludgeoning', init=1),
+         Group(id_=2, team='Infection', units=4485, unit_hp=2961,
+               weak=['fire', 'cold'], immune=['radiation'],
+               dmg=12, dmg_type='slashing', init=4)]
+    )
+
+
+@pytest.mark.parametrize("s, traits", (
+        ('', {'weak': [], 'immune': []}),
+        ('weak to radiation',
+         {'weak': ['radiation'], 'immune': []}),
+        ('weak to radiation, bludgeoning',
+         {'weak': ['radiation', 'bludgeoning'], 'immune': []}),
+        ('immune to cold',
+         {'weak': [], 'immune': ['cold']}),
+        ('immune to cold, radiation',
+         {'weak': [], 'immune': ['cold', 'radiation']}),
+        ('weak to radiation; immune to cold',
+         {'weak': ['radiation'], 'immune': ['cold']}),
+        ('weak to radiation, bludgeoning; immune to cold, slashing',
+         {'weak': ['radiation', 'bludgeoning'],
+          'immune': ['cold', 'slashing']})
+))
+def test_parse_traits(s, traits):
+    assert parse_traits(s) == traits
+
+
+def test_combat(sample):
+    assert combat(*sample) == 5216
+
+
+def test_solve():
+    assert solve() == 14000
