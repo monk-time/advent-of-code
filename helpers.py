f15caaec7ff21d34316838b7dde9a70afb022b2b
monk-time <monk-time@users.noreply.github.com>
2022-10-10 12:58:05 +0300

    Make helpers.py year-independent

--- helpers.py
+++ helpers.py
@@ -5,20 +5,26 @@ from pathlib import Path
 from typing import List, Optional
 
 
-def read_puzzle(number: Optional[int] = None, stripchars=None) -> str:
-    """Read the contents of the input file for the current puzzle."""
-    # The number of the puzzle is determined by the filename of the caller
+def read_puzzle(number: Optional[int] = None, year: Optional[int] = 2018,
+                stripchars=None) -> str:
+    """Read the contents of the input file for the current puzzle.
+
+    The number of the puzzle is determined by the filename of the caller,
+    or can be specified explicitly.
+    """
     if number:
-        year_dir = Path('year2018')
+        year_dir = Path(f'year{year}')
     else:
-        caller: str = Path(inspect.stack()[1].filename).stem.replace('test_', '')
+        caller_path: Path = Path(inspect.stack()[1].filename)
+        caller: str = caller_path.stem.replace('test_', '')
         match = re.search(r'\d+', caller)
         if not match:
             raise FileNotFoundError('Cannot find the relevant puzzle input file. '
                                     'Make sure this function is called from a file '
                                     'with a puzzle number in the filename.')
         number = int(match.group())
-        year_dir = Path(__file__).parent
+        year_dir = next(parent for parent in caller_path.parents
+                        if 'year' in parent.stem)
     puzzle_input = year_dir / 'inputs' / f'aoc{number:02d}.txt'
     return puzzle_input.read_text(encoding='utf-8').strip(stripchars)
 
