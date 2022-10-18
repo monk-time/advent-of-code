6e117e8f0e036c2b2c1db3f8660e59c70ad226cd
monk-time <monk-time@users.noreply.github.com>
2022-10-18 21:46:18 +0300

    Day 02: Part 2

--- year2019/aoc02.py
+++ year2019/aoc02.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from helpers import read_puzzle
 
 
@@ -9,7 +11,7 @@ Intcode = list[int]
 
 
 def run_intcode(program: Intcode) -> Intcode:
-    program = program[:]  # mutate only the copy of the program
+    """Execute the Intcode program. Mutates the program."""
     i = 0  # position in the program to execute
     while True:
         match program[i]:
@@ -26,14 +28,24 @@ def run_intcode(program: Intcode) -> Intcode:
         i += 4
 
 
-def prerun(program: Intcode) -> Intcode:
-    return [program[0], 12, 2, *program[3:]]
+def init(program: Intcode, noun: int, verb: int) -> Intcode:
+    return [program[0], noun, verb, *program[3:]]
+
+
+def find_input(program):
+    noun, verb = 0, 0  # PyCharm complains without this line
+    for noun, verb in product(range(100), repeat=2):
+        output = run_intcode(init(program, noun, verb))[0]
+        if output == 19690720:
+            break
+    return 100 * noun + verb
 
 
 def solve() -> tuple[int, int]:
     puzzle = parse(read_puzzle())
-    part1 = run_intcode(prerun(puzzle))[0]
-    return part1, 0
+    part1 = run_intcode(init(puzzle, 12, 2))[0]
+    part2 = find_input(puzzle)
+    return part1, part2
 
 
 if __name__ == '__main__':
