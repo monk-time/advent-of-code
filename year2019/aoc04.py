c566d48f48c7dc3ed390bdf228e2372c83d4b55f
monk-time <monk-time@users.noreply.github.com>
2022-10-20 16:33:54 +0300

    Day 04

---
+++ year2019/aoc04.py
@@ -0,0 +1,30 @@
+import re
+
+from helpers import read_puzzle
+
+
+def parse(s: str) -> tuple[int, ...]:
+    return tuple(int(n) for n in s.split('-'))
+
+
+def is_password(n: int, strict: bool = False) -> bool:
+    s = str(n)
+    runs = [run[0] for run in re.findall(r'((\d)\2+)', s)]
+    if strict:
+        runs = [run for run in runs if len(run) == 2]
+    return bool(runs) and sorted(s) == list(s)
+
+
+def count_in_range(from_: int, to: int, strict: bool = False) -> int:
+    return sum(1 for n in range(from_, to + 1) if is_password(n, strict))
+
+
+def solve() -> tuple[int, int]:
+    from_, to = parse(read_puzzle())
+    part1 = count_in_range(from_, to)
+    part2 = count_in_range(from_, to, True)
+    return part1, part2
+
+
+if __name__ == '__main__':
+    print(solve())
---
+++ year2019/tests/test_aoc04.py
@@ -0,0 +1,5 @@
+from year2019.aoc04 import solve
+
+
+def test_solve():
+    assert solve() == (544, 334)
