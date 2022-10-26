9293eb4c9bbe9fa25d5c487a8b6bafd299bfaa53
monk-time <monk-time@users.noreply.github.com>
2022-10-26 12:28:53 +0300

    Day 06: Switch to networkx

--- year2019/aoc06.py
+++ year2019/aoc06.py
@@ -1,4 +1,7 @@
+from networkx import Graph
+
 from helpers import read_puzzle
+import networkx as nx
 
 MapData = list[tuple[str, ...]]
 Node = tuple[str, int]
@@ -8,57 +11,17 @@ def parse(s: str) -> MapData:
     return [tuple(line.split(')')) for line in s.split()]
 
 
-def create_world_structure(m: MapData):
-    children, parents = {}, {}
-    for parent, child in m:
-        if parent not in children:
-            children[parent] = {child}
-        else:
-            children[parent].add(child)
-        if child not in parents:
-            parents[child] = parent
-    return children, parents
+def count_orbits(g: Graph) -> int:
+    return sum(nx.shortest_path_length(g, x, 'COM') for x in g.nodes)
 
 
-def count_orbits(m: MapData) -> int:
-    children, parents = create_world_structure(m)
-    count, depth = 0, 0
-    planets = {'COM'}
-    while True:
-        next_planets = []
-        depth += 1
-        for planet in planets:
-            pl_children = children.get(planet, set())
-            if not pl_children:
-                continue
-            count += len(pl_children) * depth
-            next_planets += list(pl_children)
-        if not next_planets:
-            return count
-        planets = next_planets
-
-
-def count_min_transfers(m: MapData) -> int:
-    children, parents = create_world_structure(m)
-    depth = 0
-    planets = {'YOU'}
-    visited = set()
-    while True:
-        next_planets = set()
-        depth += 1
-        for planet in planets:
-            visited.add(planet)
-            pl_children = children.get(planet, set())
-            pl_parent = parents.get(planet, None)
-            next_planets |= (pl_children | {pl_parent}) - visited
-        if 'SAN' in next_planets:
-            return depth - 2  # not counting first moves from YOU and SAN
-        planets = next_planets
+def count_min_transfers(g: Graph) -> int:
+    return nx.shortest_path_length(g, 'YOU', 'SAN') - 2
 
 
 def solve() -> tuple[int, int]:
-    puzzle = parse(read_puzzle())
-    return count_orbits(puzzle), count_min_transfers(puzzle)
+    g = nx.Graph(parse(read_puzzle()))
+    return count_orbits(g), count_min_transfers(g)
 
 
 if __name__ == '__main__':
--- year2019/tests/test_aoc06.py
+++ year2019/tests/test_aoc06.py
@@ -1,15 +1,18 @@
-from year2019.aoc06 import count_min_transfers, count_orbits, parse, solve
+import networkx as nx
 
+from year2019.aoc06 import count_min_transfers, count_orbits, parse, solve
 
 
 def test_count_orbits():
     sample = 'COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L'
-    assert count_orbits(parse(sample)) == 42
+    g = nx.Graph(parse(sample))
+    assert count_orbits(g) == 42
 
 
 def test_count_min_transfers():
     sample = 'COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN'
-    assert count_min_transfers(parse(sample)) == 4
+    g = nx.Graph(parse(sample))
+    assert count_min_transfers(g) == 4
 
 
 def test_solve():
