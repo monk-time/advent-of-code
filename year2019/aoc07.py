0e95ebf2eb86a41475346f4a8f440acd6c170a0a
monk-time <monk-time@users.noreply.github.com>
2022-10-27 18:37:04 +0300

    Day 07: Part 2

--- year2019/aoc07.py
+++ year2019/aoc07.py
@@ -11,6 +11,10 @@ def parse(s: str) -> list[int]:
 Intcode = list[int]
 
 
+class AmplifierHalt(Exception):
+    pass
+
+
 def nth_digit(n: int, pos: int) -> int:
     return n // (10 ** (pos - 1)) % 10
 
@@ -24,11 +28,10 @@ def get_parameters(program, i):
     return a, b
 
 
-def run_intcode(program: Intcode, phase: int, input_value: int) -> int:
+def run_intcode(program: Intcode):
     """Execute the Intcode program."""
     i = 0  # position in the program to execute
     program = program[:]
-    consumed_inputs = 0
     while True:
         op = program[i] % 100
         match op:
@@ -41,21 +44,14 @@ def run_intcode(program: Intcode, phase: int, input_value: int) -> int:
                     program[c] = a * b
                 i += 4
             case 3:  # input
-                match consumed_inputs:
-                    case 0:
-                        program[program[i + 1]] = phase
-                    case 1:
-                        program[program[i + 1]] = input_value
-                    case _:
-                        raise Exception('A program is expected to receive only two inputs')
-                consumed_inputs += 1
+                program[program[i + 1]] = yield
                 i += 2
             case 4:  # output
                 a = program[i + 1]
                 if nth_digit(program[i], 3) == 0:
                     a = program[a]
                 i += 2
-                return a
+                yield a
             case 5:  # jump-if-true
                 a, b = get_parameters(program, i)
                 i = b if a else i + 3
@@ -71,46 +67,51 @@ def run_intcode(program: Intcode, phase: int, input_value: int) -> int:
                 program[program[i + 3]] = 1 if a == b else 0
                 i += 4
             case 99:
-                raise Exception(f'A program should return a value instead of exiting')
+                raise AmplifierHalt
             case _:
                 raise Exception(f'An unknown opcode {op} at {i=}')
 
 
 def run_amplifiers(program: Intcode, phases: tuple[int, ...], initial_input=0) -> int:
-    output = run_intcode(program, phases[0], initial_input)
-    output = run_intcode(program, phases[1], output)
-    output = run_intcode(program, phases[2], output)
-    output = run_intcode(program, phases[3], output)
-    output = run_intcode(program, phases[4], output)
+    output = initial_input
+    for i in range(5):
+        gen = run_intcode(program)
+        next(gen)
+        gen.send(phases[i])
+        output = gen.send(output)
     return output
 
 
-def find_max_output(program: Intcode):
+def find_max_output(program: Intcode, use_loop=False):
     max_output = -inf
-    for phases in permutations(range(5)):
-        output = run_amplifiers(program, phases)
+    run = run_amplifiers_loop if use_loop else run_amplifiers
+    phase_range = range(5, 10) if use_loop else range(5)
+    for phases in permutations(phase_range):
+        output = run(program, phases)
         if output > max_output:
-            print(f'New maximum {output=} with {phases=}')
             max_output = output
     return max_output
 
 
-def feedback_loop(program: Intcode):
-    max_output = -inf
-    initial_input = 0
-    for phases in permutations(range(5, 10)):
-        while True:
-            initial_input = run_amplifiers(program, phases, initial_input)
-        if output > max_output:
-            print(f'New maximum {output=} with {phases=}')
-            max_output = output
-    return max_output
+def run_amplifiers_loop(program: Intcode, phases: tuple[int, ...], initial_input=0) -> int:
+    output = initial_input
+    gens = [run_intcode(program) for _ in range(5)]
+    for i in range(5):
+        next(gens[i])
+        gens[i].send(phases[i])
+    while True:
+        for i in range(5):
+            try:
+                output = gens[i].send(output)
+                next(gens[i])
+            except AmplifierHalt:
+                if i == 4:
+                    return output
 
 
 def solve():
     program = parse(read_puzzle())
-    part1 = find_max_output(program)
-    return find_max_output(program), feedback_loop(program)
+    return find_max_output(program), find_max_output(program, use_loop=True)
 
 
 if __name__ == '__main__':
