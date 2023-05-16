da695dffc8a5779d558306fdbbf3a74f191c5022
Dmitry <monk-time@users.noreply.github.com>
2023-05-16 21:12:40 +0300

    Day 16: Partial refactor

--- year2019/aoc16.py
+++ year2019/aoc16.py
@@ -1,14 +1,14 @@
 from itertools import chain, cycle, islice, repeat, starmap
 from operator import mul
-from helpers import read_puzzle
+from helpers import read_puzzle, timed
 
 PATTERN = (0, 1, 0, -1)
 
-Signal = tuple[int]
+Signal = list[int]
 
 
 def parse(s: str) -> Signal:
-    return tuple(int(ch) for ch in s)
+    return [int(ch) for ch in s]
 
 
 def to_str(signal: Signal) -> str:
@@ -29,6 +29,22 @@ def apply_phase_of_fft(signal: Signal) -> int:
     return tuple(get_digit(i) for i in range(n))
 
 
+def apply_phase_of_fft(signal: Signal) -> int:
+    result = []
+    for i in range(1, len(signal) + 1):
+        positive_sum = 0
+        for j in range(i):
+            for k in range(j + i - 1, len(signal), i * 4):
+                positive_sum += signal[k]
+        negative_sum = 0
+        for j in range(i):
+            for k in range(j + 3 * i - 1, len(signal), i * 4):
+                negative_sum += signal[k]
+        digit = abs(positive_sum - negative_sum) % 10
+        result.append(digit)
+    return result
+
+
 def apply_n_phases(signal: Signal, n: int) -> Signal:
     for _ in range(n):
         signal = apply_phase_of_fft(signal)
@@ -42,6 +58,7 @@ def real_signal(signal: Signal) -> Signal:
     return result_signal[offset : offset + 8]
 
 
+@timed
 def solve() -> tuple[int, int]:
     signal = parse(read_puzzle())
     part1 = to_str(apply_n_phases(signal, 100))
