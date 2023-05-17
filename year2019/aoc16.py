24092665b69b99f193d1c636712991f9769c6caf
Dmitry <monk-time@users.noreply.github.com>
2023-05-17 15:57:18 +0300

    Day 16: Part 2

--- year2019/aoc16.py
+++ year2019/aoc16.py
@@ -1,14 +1,15 @@
-from itertools import chain, cycle, islice, repeat, starmap
+from itertools import accumulate, chain, cycle, islice, repeat, starmap
 from operator import mul
+
 from helpers import read_puzzle, timed
 
 PATTERN = (0, 1, 0, -1)
 
-Signal = list[int]
+Signal = tuple[int]
 
 
 def parse(s: str) -> Signal:
-    return [int(ch) for ch in s]
+    return tuple(int(ch) for ch in s)
 
 
 def to_str(signal: Signal) -> str:
@@ -18,45 +19,18 @@ def to_str(signal: Signal) -> str:
 def pattern(length: int, times: int) -> Signal:
     repeated = (repeat(n, times) for n in PATTERN)
     looped = cycle(chain.from_iterable(repeated))
-    return list(islice(looped, 1, length + 1))
+    return tuple(islice(looped, 1, length + 1))
 
 
-def get_patterns(length: int) -> list[Signal]:
-    return [pattern(length, i) for i in range(1, length + 1)]
+def get_patterns(length: int) -> tuple[Signal]:
+    return tuple(pattern(length, i) for i in range(1, length + 1))
 
 
-def apply_phase_of_fft(signal: Signal, patterns: list[Signal]) -> int:
+def apply_phase_of_fft(signal: Signal, patterns: tuple[Signal]) -> Signal:
     n = len(signal)
     apply_pattern = lambda i: starmap(mul, zip(signal[i:], patterns[i][i:]))
     get_digit = lambda i: abs(sum(apply_pattern(i))) % 10
-    return [get_digit(i) for i in range(n)]
-
-
-def apply_phase_of_fft2(signal: Signal, patterns: list[Signal]) -> int:
-    n = len(signal)
-    new_signal = []
-    for digit in range(n):
-        new_digit = 0
-        for i in range(digit, n):
-            new_digit += signal[i] * patterns[digit][i]
-        new_signal.append(abs(new_digit) % 10)
-    return new_signal
-
-
-def apply_phase_of_fft3(signal: Signal, patterns: list[Signal]) -> int:
-    result = []
-    for i in range(1, len(signal) + 1):
-        positive_sum = 0
-        for j in range(i):
-            for k in range(j + i - 1, len(signal), i * 4):
-                positive_sum += signal[k]
-        negative_sum = 0
-        for j in range(i):
-            for k in range(j + 3 * i - 1, len(signal), i * 4):
-                negative_sum += signal[k]
-        digit = abs(positive_sum - negative_sum) % 10
-        result.append(digit)
-    return result
+    return tuple(get_digit(i) for i in range(n))
 
 
 def apply_n_phases(signal: Signal, n: int) -> Signal:
@@ -69,16 +43,18 @@ def apply_n_phases(signal: Signal, n: int) -> Signal:
 def real_signal(signal: Signal) -> Signal:
     input_signal = signal * 10_000
     offset = int(to_str(signal[:7]))
-    result_signal = apply_n_phases(input_signal[offset:], 100)
-    return result_signal[:8]
+    signal = reversed(input_signal[offset:])
+    for _ in range(100):
+        signal = accumulate(signal, lambda a, b: (a + b) % 10)
+    return tuple(islice(reversed(tuple(signal)), 8))
 
 
 @timed
 def solve() -> tuple[int, int]:
     signal = parse(read_puzzle())
     part1 = to_str(apply_n_phases(signal, 100))
-    # part2 = to_str(real_signal(signal))
-    return part1, 0
+    part2 = to_str(real_signal(signal))
+    return part1, part2
 
 
 if __name__ == '__main__':
