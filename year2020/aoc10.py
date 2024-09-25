84560239776b99fe93f70790fd9a259af5216d71
Dmitry <monk-time@users.noreply.github.com>
2024-09-26 00:52:24 +0300

    Day 10

--- year2020/aoc10.py
+++ year2020/aoc10.py
@@ -1,5 +1,8 @@
 # https://adventofcode.com/2020/day/10
 
+from collections import Counter, defaultdict
+from itertools import pairwise
+
 from helpers import read_puzzle
 
 
@@ -7,9 +10,24 @@ def parse(s: str) -> list[int]:
     return [int(line) for line in s.split()]
 
 
+def difference(arr: list[int]) -> int:
+    arr = sorted(arr)
+    c = Counter(b - a for a, b in pairwise([0, *arr, arr[-1] + 3]))
+    return c[1] * c[3]
+
+
+def count_chains(arr: list[int]) -> int:
+    arr = sorted(arr)
+    max_value = arr[-1] + 3
+    counts = defaultdict(int, {0: 1})
+    for n in [*arr, max_value]:
+        counts[n] = sum(counts[n - 1 - d] for d in range(3))
+    return counts[max_value]
+
+
 def solve() -> tuple[int, int]:
-    _puzzle = parse(read_puzzle())
-    return 0, 0
+    arr = parse(read_puzzle())
+    return difference(arr), count_chains(arr)
 
 
 if __name__ == '__main__':
--- year2020/tests/test_aoc10.py
+++ year2020/tests/test_aoc10.py
@@ -1,5 +1,52 @@
-from year2020.aoc10 import solve
+import pytest
+
+from year2020.aoc10 import count_chains, difference, solve
+
+sample1 = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+sample2 = [
+    28,
+    33,
+    18,
+    42,
+    31,
+    14,
+    46,
+    20,
+    48,
+    47,
+    24,
+    23,
+    49,
+    45,
+    19,
+    38,
+    39,
+    11,
+    1,
+    32,
+    25,
+    35,
+    8,
+    17,
+    7,
+    9,
+    4,
+    2,
+    34,
+    10,
+    3,
+]
+
+
+@pytest.mark.parametrize('arr, result', ((sample1, 35), (sample2, 220)))
+def test_difference(arr, result):
+    assert difference(arr) == result
+
+
+@pytest.mark.parametrize('arr, result', ((sample1, 8), (sample2, 19208)))
+def test_count_chains(arr, result):
+    assert count_chains(arr) == result
 
 
 def test_solve():
-    assert solve() == (0, 0)
+    assert solve() == (2368, 1727094849536)
