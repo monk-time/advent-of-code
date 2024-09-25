2bc1bf976ac12e9b1a0663cab3a0f2d4fd99a4bc
Dmitry <monk-time@users.noreply.github.com>
2024-09-26 00:52:46 +0300

    Day 11

--- year2020/aoc11.py
+++ year2020/aoc11.py
@@ -1,15 +1,84 @@
 # https://adventofcode.com/2020/day/11
 
+from collections.abc import Callable, Iterable
+from itertools import product
+
 from helpers import read_puzzle
 
+type Coord = tuple[int, int]
+type Grid = tuple[tuple[str, ...], ...]
+type AdjMap = dict[Coord, tuple[Coord, ...]]
+type NeighborFunc = Callable[[Coord, Grid], Iterable[Coord]]
 
-def parse(s: str) -> list[int]:
-    return [int(line) for line in s.split()]
+
+def parse(s: str) -> Grid:
+    return tuple(tuple(line) for line in s.split('\n'))
+
+
+def neighbors_1(c: Coord, grid: Grid) -> Iterable[Coord]:
+    h, w = len(grid), len(grid[0])
+    i, j = c
+    for di, dj in product(range(-1, 2), repeat=2):
+        if di == dj == 0:
+            continue
+        i2, j2 = i + di, j + dj
+        if not (0 <= i2 < h and 0 <= j2 < w) or grid[i2][j2] == '.':
+            continue
+        yield (i2, j2)
+
+
+def neighbors_2(c: Coord, grid: Grid) -> Iterable[Coord]:
+    h, w = len(grid), len(grid[0])
+    i, j = c
+    for di, dj in product(range(-1, 2), repeat=2):
+        if di == dj == 0:
+            continue
+        i2, j2 = i, j
+        while 0 <= (i2 := i2 + di) < h and 0 <= (j2 := j2 + dj) < w:
+            if grid[i2][j2] != '.':
+                yield (i2, j2)
+                break
+
+
+def get_adj_map(grid: Grid, neighbors: NeighborFunc) -> AdjMap:
+    h, w = len(grid), len(grid[0])
+    return {c: tuple(neighbors(c, grid)) for c in product(range(h), range(w))}
+
+
+def update_seat(i: int, j: int, grid: Grid, adj_map: AdjMap, k: int) -> str:
+    if (seat := grid[i][j]) == '.':
+        return '.'
+    occupied_count = 0
+    for i2, j2 in adj_map[i, j]:
+        occupied_count += grid[i2][j2] == '#'
+        if (seat == 'L' and occupied_count > 0) or occupied_count == k:
+            return 'L'
+    return '#'
+
+
+def iterate(grid: Grid, adj_map: AdjMap, k: int) -> Grid:
+    h, w = len(grid), len(grid[0])
+    return tuple(
+        tuple(update_seat(i, j, grid, adj_map, k) for j in range(w))
+        for i in range(h)
+    )
+
+
+def find_loop(grid: Grid, neighbors: NeighborFunc, k: int) -> int:
+    adj_map = get_adj_map(grid, neighbors)
+    cache: set[Grid] = set()
+    while grid not in cache:
+        cache.add(grid)
+        grid = iterate(grid, adj_map, k)
+    return sum(grid[i].count('#') for i in range(len(grid)))
 
 
 def solve() -> tuple[int, int]:
-    _puzzle = parse(read_puzzle())
-    return 0, 0
+    grid = parse(read_puzzle())
+    return (
+        find_loop(grid, neighbors_1, 4),
+        find_loop(grid, neighbors_2, 5),
+    )
 
 
 if __name__ == '__main__':
--- year2020/tests/test_aoc11.py
+++ year2020/tests/test_aoc11.py
@@ -1,5 +1,207 @@
-from year2020.aoc11 import solve
+from inspect import cleandoc
+
+import pytest
+
+from year2020.aoc11 import (
+    Grid,
+    find_loop,
+    get_adj_map,
+    iterate,
+    neighbors_1,
+    neighbors_2,
+    parse,
+    solve,
+)
+
+sample1 = cleandoc("""
+    L.LL.LL.LL
+    LLLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLLL
+    L.LLLLLL.L
+    L.LLLLL.LL
+""")
+
+sample2_1 = cleandoc("""
+    #.##.##.##
+    #######.##
+    #.#.#..#..
+    ####.##.##
+    #.##.##.##
+    #.#####.##
+    ..#.#.....
+    ##########
+    #.######.#
+    #.#####.##
+""")
+
+sample3_1 = cleandoc("""
+    #.LL.L#.##
+    #LLLLLL.L#
+    L.L.L..L..
+    #LLL.LL.L#
+    #.LL.LL.LL
+    #.LLLL#.##
+    ..L.L.....
+    #LLLLLLLL#
+    #.LLLLLL.L
+    #.#LLLL.##
+""")
+
+sample4_1 = cleandoc("""
+    #.##.L#.##
+    #L###LL.L#
+    L.#.#..#..
+    #L##.##.L#
+    #.##.LL.LL
+    #.###L#.##
+    ..#.#.....
+    #L######L#
+    #.LL###L.L
+    #.#L###.##
+""")
+
+sample5_1 = cleandoc("""
+    #.#L.L#.##
+    #LLL#LL.L#
+    L.L.L..#..
+    #LLL.##.L#
+    #.LL.LL.LL
+    #.LL#L#.##
+    ..L.L.....
+    #L#LLLL#L#
+    #.LLLLLL.L
+    #.#L#L#.##
+""")
+
+sample6_1 = cleandoc("""
+    #.#L.L#.##
+    #LLL#LL.L#
+    L.#.L..#..
+    #L##.##.L#
+    #.#L.LL.LL
+    #.#L#L#.##
+    ..L.L.....
+    #L#L##L#L#
+    #.LLLLLL.L
+    #.#L#L#.##
+""")
+
+sample2_2 = cleandoc("""
+    #.##.##.##
+    #######.##
+    #.#.#..#..
+    ####.##.##
+    #.##.##.##
+    #.#####.##
+    ..#.#.....
+    ##########
+    #.######.#
+    #.#####.##
+""")
+
+sample3_2 = cleandoc("""
+    #.LL.LL.L#
+    #LLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLL#
+    #.LLLLLL.L
+    #.LLLLL.L#
+""")
+
+sample4_2 = cleandoc("""
+    #.L#.##.L#
+    #L#####.LL
+    L.#.#..#..
+    ##L#.##.##
+    #.##.#L.##
+    #.#####.#L
+    ..#.#.....
+    LLL####LL#
+    #.L#####.L
+    #.L####.L#
+""")
+
+
+sample5_2 = cleandoc("""
+    #.L#.L#.L#
+    #LLLLLL.LL
+    L.L.L..#..
+    ##LL.LL.L#
+    L.LL.LL.L#
+    #.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLL#
+    #.LLLLL#.L
+    #.L#LL#.L#
+""")
+
+sample6_2 = cleandoc("""
+    #.L#.L#.L#
+    #LLLLLL.LL
+    L.L.L..#..
+    ##L#.#L.L#
+    L.L#.#L.L#
+    #.L####.LL
+    ..#.#.....
+    LLL###LLL#
+    #.LLLLL#.L
+    #.L#LL#.L#
+""")
+
+sample7_2 = cleandoc("""
+    #.L#.L#.L#
+    #LLLLLL.LL
+    L.L.L..#..
+    ##L#.#L.L#
+    L.L#.LL.L#
+    #.LLLL#.LL
+    ..#.L.....
+    LLL###LLL#
+    #.LLLLL#.L
+    #.L#LL#.L#
+""")
+
+
+def grid_to_str(grid: Grid) -> str:
+    h = len(grid)
+    return '\n'.join(''.join(grid[i]) for i in range(h))
+
+
+@pytest.mark.parametrize(
+    'neighbors, k, grid, next_grid',
+    (
+        (neighbors_1, 4, sample1, sample2_1),
+        (neighbors_1, 4, sample2_1, sample3_1),
+        (neighbors_1, 4, sample3_1, sample4_1),
+        (neighbors_1, 4, sample4_1, sample5_1),
+        (neighbors_1, 4, sample5_1, sample6_1),
+        (neighbors_2, 5, sample1, sample2_2),
+        (neighbors_2, 5, sample2_2, sample3_2),
+        (neighbors_2, 5, sample3_2, sample4_2),
+        (neighbors_2, 5, sample4_2, sample5_2),
+        (neighbors_2, 5, sample5_2, sample6_2),
+        (neighbors_2, 5, sample6_2, sample7_2),
+    ),
+)
+def test_iterate(neighbors, k, grid, next_grid):
+    grid = parse(grid)
+    adj_map = get_adj_map(grid, neighbors)
+    assert grid_to_str(iterate(grid, adj_map, k)) == next_grid
+
+
+def test_find_loop():
+    assert find_loop(parse(sample1), neighbors_1, 4) == 37
+    assert find_loop(parse(sample1), neighbors_2, 5) == 26
 
 
 def test_solve():
-    assert solve() == (0, 0)
+    assert solve() == (2299, 2047)
