2eaf8a1ad733535d9ebecf04ab18a4a9e08c9585
Dmitry <monk-time@users.noreply.github.com>
2024-09-28 03:50:12 +0300

    Day 12

--- year2020/aoc12.py
+++ year2020/aoc12.py
@@ -1,15 +1,48 @@
 # https://adventofcode.com/2020/day/12
 
+from collections.abc import Callable
+from functools import partial
+
 from helpers import read_puzzle
 
+type Move = tuple[str, int]
+type Coord = complex
+type Actions = dict[str, Callable[[int, Coord, Coord], tuple[Coord, Coord]]]
 
-def parse(s: str) -> list[int]:
-    return [int(line) for line in s.split()]
+ACTIONS: Actions = {
+    'N': lambda n, c, waypoint: (c + n * 1j, waypoint),
+    'S': lambda n, c, waypoint: (c - n * 1j, waypoint),
+    'E': lambda n, c, waypoint: (c + n, waypoint),
+    'W': lambda n, c, waypoint: (c - n, waypoint),
+    'L': lambda n, c, waypoint: (c, waypoint * ((+1j) ** (n // 90))),
+    'R': lambda n, c, waypoint: (c, waypoint * ((-1j) ** (n // 90))),
+    'F': lambda n, c, waypoint: (c + n * waypoint, waypoint),
+}
+
+
+def parse(s: str) -> list[Move]:
+    return [(line[0], int(line[1:])) for line in s.split()]
+
+
+def execute(
+    moves: list[Move], waypoint: Coord, *, move_waypoint: bool = False
+) -> int:
+    c = 0j
+    for action, n in moves:
+        if move_waypoint and action in 'NSEW':
+            waypoint, c = ACTIONS[action](n, waypoint, c)
+        else:
+            c, waypoint = ACTIONS[action](n, c, waypoint)
+    return int(abs(c.real) + abs(c.imag))
+
+
+part1 = partial(execute, waypoint=1 + 0j)
+part2 = partial(execute, waypoint=10 + 1j, move_waypoint=True)
 
 
 def solve() -> tuple[int, int]:
-    _puzzle = parse(read_puzzle())
-    return 0, 0
+    moves = parse(read_puzzle())
+    return part1(moves), part2(moves)
 
 
 if __name__ == '__main__':
--- year2020/tests/test_aoc12.py
+++ year2020/tests/test_aoc12.py
@@ -1,5 +1,23 @@
-from year2020.aoc12 import solve
+from inspect import cleandoc
+
+from year2020.aoc12 import parse, part1, part2, solve
+
+sample = cleandoc("""
+    F10
+    N3
+    F7
+    R90
+    F11
+""")
+
+
+def test_part1():
+    assert part1(parse(sample)) == 25
+
+
+def test_part2():
+    assert part2(parse(sample)) == 286
 
 
 def test_solve():
-    assert solve() == (0, 0)
+    assert solve() == (1710, 62045)
