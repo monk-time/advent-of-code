c0c21db778ce64b64ab19e8443bf0fc98801c58f
Dmitry <monk-time@users.noreply.github.com>
2025-03-12 19:03:19 +0300

    Day 16

--- year2020/aoc16.py
+++ year2020/aoc16.py
@@ -1,15 +1,93 @@
 # https://adventofcode.com/2020/day/16
 
+import re
+from dataclasses import dataclass
+from functools import partial
+from itertools import batched, chain, pairwise
+from math import prod
+
 from helpers import read_puzzle
 
+type Range = tuple[int, int]
+type Fields = tuple[int, ...]
+type Rule = tuple[Range, Range]
+type Rules = tuple[Rule, ...]
 
-def parse(s: str) -> list[int]:
-    return [int(line) for line in s.split()]
+
+@dataclass(frozen=True)
+class TicketData:
+    rules: Rules
+    ticket: Fields
+    tickets: tuple[Fields, ...]
+
+
+def parse(s: str) -> TicketData:
+    parts = s.split('\n\n')
+    find_ints = lambda arr: map(int, re.findall(r'\d+', arr))
+    rules: Rules = tuple(
+        batched(batched(find_ints(parts[0]), 2, strict=True), 2, strict=True)
+    )  # type: ignore
+    ticket = tuple(find_ints(parts[1]))
+    tickets = tuple(
+        tuple(find_ints(line)) for line in parts[2].split('\n')[1:]
+    )
+    return TicketData(rules, ticket, tickets)
+
+
+def matches_rule(rule: Rule, n: int) -> bool:
+    (a1, a2), (b1, b2) = rule
+    return a1 <= n <= a2 or b1 <= n <= b2
+
+
+def is_invalid_field(rules: Rules, n: int) -> bool:
+    return not any(matches_rule(rule, n) for rule in rules)
+
+
+def find_invalid_fields(rules: Rules, ticket: Fields) -> Fields:
+    return tuple(n for n in ticket if is_invalid_field(rules, n))
+
+
+def calc_ticket_scanning_error_rate(td: TicketData) -> int:
+    func = partial(find_invalid_fields, td.rules)
+    return sum(chain(*map(func, td.tickets)))
+
+
+def find_candidate_cols(
+    rule: Rule, tickets: tuple[Fields, ...]
+) -> tuple[int, ...]:
+    return tuple(
+        i
+        for i in range(len(tickets[0]))
+        if all(matches_rule(rule, t[i]) for t in tickets)
+    )
+
+
+def mul_departure_fields(td: TicketData) -> int:
+    valid_tickets = tuple(
+        t
+        for t in chain([td.ticket], td.tickets)
+        if not find_invalid_fields(td.rules, t)
+    )
+    candidates = sorted(
+        (
+            (rule_idx, find_candidate_cols(td.rules[rule_idx], valid_tickets))
+            for rule_idx in range(len(td.rules))
+        ),
+        key=lambda t: len(t[1]),
+    )
+    rule_idx, cols = candidates[0]
+    rule_cols: list[int] = [0] * len(td.rules)
+    rule_cols[rule_idx] = cols[0]
+    # Assumption: there's no branching
+    # and the number of candidates strictly increases
+    for (_, prev), (rule_idx, cols) in pairwise(candidates):
+        rule_cols[rule_idx] = next(iter(set(cols) - set(prev)))
+    return prod(td.ticket[col] for col in rule_cols[:6])
 
 
 def solve() -> tuple[int, int]:
-    _puzzle = parse(read_puzzle())
-    return 0, 0
+    td = parse(read_puzzle())
+    return calc_ticket_scanning_error_rate(td), mul_departure_fields(td)
 
 
 if __name__ == '__main__':
--- year2020/tests/test_aoc16.py
+++ year2020/tests/test_aoc16.py
@@ -1,5 +1,53 @@
-from year2020.aoc16 import solve
+from inspect import cleandoc
+
+from year2020.aoc16 import (
+    TicketData,
+    calc_ticket_scanning_error_rate,
+    parse,
+    solve,
+)
+
+input_1 = cleandoc("""
+    class: 1-3 or 5-7
+    row: 6-11 or 33-44
+    seat: 13-40 or 45-50
+
+    your ticket:
+    7,1,14
+
+    nearby tickets:
+    7,3,47
+    40,4,50
+    55,2,20
+    38,6,12
+""")
+
+input_2 = cleandoc("""
+    class: 0-1 or 4-19
+    row: 0-5 or 8-19
+    seat: 0-13 or 16-19
+
+    your ticket:
+    11,12,13
+
+    nearby tickets:
+    3,9,18
+    15,1,5
+    5,14,9
+""")
+
+
+def test_parse():
+    assert parse(input_1) == TicketData(
+        rules=(((1, 3), (5, 7)), ((6, 11), (33, 44)), ((13, 40), (45, 50))),
+        ticket=(7, 1, 14),
+        tickets=((7, 3, 47), (40, 4, 50), (55, 2, 20), (38, 6, 12)),
+    )
+
+
+def test_calc_ticket_scanning_error_rate():
+    assert calc_ticket_scanning_error_rate(parse(input_1)) == 71
 
 
 def test_solve():
-    assert solve() == (0, 0)
+    assert solve() == (27850, 491924517533)
