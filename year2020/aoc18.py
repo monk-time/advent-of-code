52f1dd66631a5358a12265ab797bc969425a514d
Dmitry <monk-time@users.noreply.github.com>
2025-03-16 18:19:27 +0300

    Day 18

--- year2020/aoc18.py
+++ year2020/aoc18.py
@@ -1,15 +1,73 @@
 # https://adventofcode.com/2020/day/18
 
+from collections.abc import Iterable
+from functools import partial
+from operator import add, mul
+
 from helpers import read_puzzle
 
+type Tokens = Iterable[int | str]
+type Precedence = dict[str, int]
 
-def parse(s: str) -> list[int]:
-    return [int(line) for line in s.split()]
+OPS = {'+': add, '*': mul}
+
+
+def parse(s: str) -> list[Tokens]:
+    return [parse_tokens(line) for line in s.split('\n')]
+
+
+def parse_tokens(s: str) -> Tokens:
+    return [int(ch) if ch.isdigit() else ch for ch in s if ch != ' ']
+
+
+def infix_to_postfix(tokens: Tokens, precedence: Precedence) -> Tokens:
+    # Shunting yard algorithm
+    stack: list[str] = []
+
+    for token in tokens:
+        if isinstance(token, int):
+            yield token
+        elif token in precedence:
+            while (
+                stack
+                and stack[-1] != '('
+                and precedence[stack[-1]] <= precedence[token]
+            ):
+                yield stack.pop()
+            stack.append(token)
+        elif token == '(':
+            stack.append(token)
+        elif token == ')':
+            while stack and stack[-1] != '(':
+                yield stack.pop()
+            stack.pop()  # remove '('
+
+    while stack:
+        yield stack.pop()
+
+
+def calc_postfix(postfix: Tokens) -> int:
+    stack = []
+    for token in postfix:
+        if isinstance(token, int):
+            stack.append(token)
+        else:
+            op = OPS[token]
+            stack.append(op(stack.pop(), stack.pop()))
+    return stack[0]
+
+
+def calc(expr: Tokens, precedence: Precedence) -> int:
+    return calc_postfix(infix_to_postfix(expr, precedence))
+
+
+calc_1 = partial(calc, precedence={'+': 1, '*': 1})
+calc_2 = partial(calc, precedence={'+': 1, '*': 2})
 
 
 def solve() -> tuple[int, int]:
-    _puzzle = parse(read_puzzle())
-    return 0, 0
+    puzzle = parse(read_puzzle())
+    return sum(map(calc_1, puzzle)), sum(map(calc_2, puzzle))
 
 
 if __name__ == '__main__':
--- year2020/tests/test_aoc18.py
+++ year2020/tests/test_aoc18.py
@@ -1,5 +1,37 @@
-from year2020.aoc18 import solve
+import pytest
+
+from year2020.aoc18 import calc_1, calc_2, parse_tokens, solve
+
+
+@pytest.mark.parametrize(
+    's, result',
+    (
+        ('1 + 2 * 3 + 4 * 5 + 6', 71),
+        ('1 + (2 * 3) + (4 * (5 + 6))', 51),
+        ('2 * 3 + (4 * 5)', 26),
+        ('5 + (8 * 3 + 9 + 3 * 4 * 3)', 437),
+        ('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', 12240),
+        ('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', 13632),
+    ),
+)
+def test_calc_1(s, result):
+    assert calc_1(parse_tokens(s)) == result
+
+
+@pytest.mark.parametrize(
+    's, result',
+    (
+        ('1 + 2 * 3 + 4 * 5 + 6', 231),
+        ('1 + (2 * 3) + (4 * (5 + 6))', 51),
+        ('2 * 3 + (4 * 5)', 46),
+        ('5 + (8 * 3 + 9 + 3 * 4 * 3)', 1445),
+        ('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', 669060),
+        ('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', 23340),
+    ),
+)
+def test_calc_2(s, result):
+    assert calc_2(parse_tokens(s)) == result
 
 
 def test_solve():
-    assert solve() == (0, 0)
+    assert solve() == (11076907812171, 283729053022731)
