c934889866b65f8a7041572062339da56da01dee
Dmitry <monk-time@users.noreply.github.com>
2025-10-25 18:16:57 +0300

    Day 19

--- year2020/aoc19.py
+++ year2020/aoc19.py
@@ -1,15 +1,82 @@
 # https://adventofcode.com/2020/day/19
 
+import re
+from collections import deque
+from itertools import chain
+
 from helpers import read_puzzle
 
+# Patterns consist of two halves separated by |
+# Each half is a sequence of either rule indices or literal characters
+type Pattern = tuple[tuple[str | int, ...], ...]
+type Rules = dict[int, Pattern]
+type ResolvedRules = dict[int, str]
 
-def parse(s: str) -> list[int]:
-    return [int(line) for line in s.split()]
+
+def soft_int(s: str) -> int | str:
+    return int(s) if s.isdigit() else s
+
+
+def parse(s: str) -> tuple[Rules, list[str]]:
+    lines, msgs = [x.splitlines() for x in s.split('\n\n')]
+    rules: Rules = {}
+    for line in lines:
+        idx, pattern = line.split(': ')
+        idx = int(idx)
+        pattern = pattern.replace('"', '')
+        halves = pattern.split(' | ')
+        rules[idx] = tuple(
+            tuple(map(soft_int, half.split())) for half in halves
+        )
+    return rules, msgs
+
+
+def resolve(rules: Rules) -> ResolvedRules:
+    queue = deque([0])
+    resolved: ResolvedRules = {}
+    while queue:
+        idx = queue.popleft()
+        if idx in resolved:
+            continue
+        cur_len = len(queue)
+        for x in chain.from_iterable(rules[idx]):
+            if isinstance(x, int) and x not in resolved:
+                queue.append(x)
+        if len(queue) != cur_len:
+            queue.append(idx)
+            continue
+        resolved[idx] = '|'.join(
+            ''.join(resolved[x] if isinstance(x, int) else x for x in half)
+            for half in rules[idx]
+        )
+        if len(rules[idx]) == 2:
+            resolved[idx] = f'({resolved[idx]})'
+    return resolved
+
+
+def count_part1(resolved: ResolvedRules, msgs: list[str]) -> int:
+    return sum(1 for msg in msgs if re.fullmatch(resolved[0], msg))
+
+
+def count_part2(resolved: ResolvedRules, msgs: list[str]) -> int:
+    # Rule 0 is always 42 42 31.
+    # With changes for part 2 it becomes 42+ 42{n} 31{n},
+    # or 42+ 31+ where 42 appears more times than 31.
+    re_x, re_y = resolved[42], resolved[31]
+    pattern = f'(?P<x>({re_x})+)(?P<y>({re_y})+)'
+    return sum(
+        1
+        for msg in msgs
+        if (m := re.fullmatch(pattern, msg))
+        and len(re.findall(re_x, m.group('x')))
+        > len(re.findall(re_y, m.group('y')))
+    )
 
 
 def solve() -> tuple[int, int]:
-    _puzzle = parse(read_puzzle())
-    return 0, 0
+    rules, msgs = parse(read_puzzle())
+    resolved = resolve(rules)
+    return (count_part1(resolved, msgs), count_part2(resolved, msgs))
 
 
 if __name__ == '__main__':
--- year2020/tests/test_aoc19.py
+++ year2020/tests/test_aoc19.py
@@ -1,5 +1,115 @@
-from year2020.aoc19 import solve
+from inspect import cleandoc
+
+import pytest
+
+from year2020.aoc19 import (
+    count_part1,
+    count_part2,
+    parse,
+    resolve,
+    solve,
+)
+
+rules_1 = cleandoc("""
+    0: 1 2
+    1: "a"
+    2: 1 3 | 3 1
+    3: "b"
+
+    aaa
+""")
+
+rules_2 = cleandoc("""
+    0: 4 1 5
+    1: 2 3 | 3 2
+    2: 4 4 | 5 5
+    3: 4 5 | 5 4
+    4: "a"
+    5: "b"
+
+    ababbb
+    bababa
+    abbbab
+    aaabbb
+    aaaabbb
+""")
+
+rules_3 = cleandoc("""
+    42: 9 14 | 10 1
+    9: 14 27 | 1 26
+    10: 23 14 | 28 1
+    1: "a"
+    11: 42 31
+    5: 1 14 | 15 1
+    19: 14 1 | 14 14
+    12: 24 14 | 19 1
+    16: 15 1 | 14 14
+    31: 14 17 | 1 13
+    6: 14 14 | 1 14
+    2: 1 24 | 14 4
+    0: 8 11
+    13: 14 3 | 1 12
+    15: 1 | 14
+    17: 14 2 | 1 7
+    23: 25 1 | 22 14
+    28: 16 1
+    4: 1 1
+    20: 14 14 | 1 15
+    3: 5 14 | 16 1
+    27: 1 6 | 14 18
+    14: "b"
+    21: 14 1 | 1 14
+    25: 1 1 | 1 14
+    22: 14 14
+    8: 42
+    26: 14 22 | 1 20
+    18: 15 15
+    7: 14 5 | 1 21
+    24: 14 1
+
+    abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+    bbabbbbaabaabba
+    babbbbaabbbbbabbbbbbaabaaabaaa
+    aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+    bbbbbbbaaaabbbbaaabbabaaa
+    bbbababbbbaaaaaaaabbababaaababaabab
+    ababaaaaaabaaab
+    ababaaaaabbbaba
+    baabbaaaabbaaaababbaababb
+    abbbbabbbbaaaababbbbbbaaaababb
+    aaaaabbaabaaaaababaa
+    aaaabbaaaabbaaa
+    aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+    babaaabbbaaabaababbaabababaaab
+    aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+""")
+
+sample_rulesets = (rules_1, rules_2, rules_3)
+sample_resolved = ('a(ab|ba)', 'a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b')
+sample_match_count = (0, 2, 3)
+
+
+@pytest.mark.parametrize(
+    'rules, result', zip(sample_rulesets, sample_resolved)
+)
+def test_resolve(rules, result):
+    rules, _ = parse(rules)
+    assert resolve(rules)[0] == result
+
+
+@pytest.mark.parametrize(
+    'rules, result', zip(sample_rulesets, sample_match_count)
+)
+def test_count_part1(rules, result):
+    rules, msgs = parse(rules)
+    assert count_part1(resolve(rules), msgs) == result
+
+
+def test_count_part2():
+    rules, msgs = parse(rules_3)
+    resolved = resolve(rules)
+    assert count_part2(resolved, msgs) == 12
 
 
 def test_solve():
-    assert solve() == (0, 0)
+    assert solve() == (269, 403)
