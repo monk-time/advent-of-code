d5ad781d339cb8052eff43b5b7203c23e108c830
Dmitry <monk-time@users.noreply.github.com>
2025-11-13 01:01:19 +0300

    Day 24

--- year2020/aoc24.py
+++ year2020/aoc24.py
@@ -1,15 +1,89 @@
 # https://adventofcode.com/2020/day/24
 
+import re
+from collections import Counter, defaultdict
+from dataclasses import dataclass, field
+from functools import cache, reduce
+from typing import Literal, Self, get_args
+
 from helpers import read_puzzle
 
+type Coord = tuple[int, int]
+# can't use `type` for Dir because of get_args()
+Dir = Literal['e', 'se', 'sw', 'w', 'nw', 'ne']
 
-def parse(s: str) -> list[int]:
-    return [int(line) for line in s.split()]
+RE_DIR = r'(?:e|se|sw|w|nw|ne)'
+
+
+def move(c: Coord, dir_: Dir) -> Coord:
+    x, y = c
+    match dir_:
+        case 'e':
+            return (x + 1, y)
+        case 'se':
+            return (x, y + 1)
+        case 'sw':
+            return (x - 1, y + 1)
+        case 'w':
+            return (x - 1, y)
+        case 'nw':
+            return (x, y - 1)
+        case 'ne':
+            return (x + 1, y - 1)
+
+
+@cache
+def around(c: Coord) -> tuple[Coord, ...]:
+    return tuple(move(c, dir_) for dir_ in get_args(Dir))
+
+
+@dataclass
+class State:
+    active: set[Coord] = field(default_factory=set)  # black tiles are active
+    has_2: set[Coord] = field(default_factory=set)  # 2 active neighbors
+    adj: defaultdict[Coord, int] = field(
+        default_factory=lambda: defaultdict(int)
+    )
+
+    @classmethod
+    def from_str(cls, s: str) -> Self:
+        guide = [re.findall(RE_DIR, line) for line in s.splitlines()]
+        counter = Counter(reduce(move, dirs, (0, 0)) for dirs in guide)
+        black_tiles = {c for c, count in counter.items() if count % 2 == 1}
+        state = cls()
+        for c in black_tiles:
+            state.toggle(c, delta=1)
+        return state
+
+    def toggle(self, c: Coord, *, delta: int) -> None:
+        for c2 in around(c):
+            self.adj[c2] += delta
+            if self.adj[c2] == 2:
+                self.has_2.add(c2)
+            else:
+                self.has_2.discard(c2)
+        if delta == 1:
+            self.active.add(c)
+        else:
+            self.active.remove(c)
+
+    def cycle(self) -> None:
+        disable = {
+            c for c in self.active if self.adj[c] == 0 or self.adj[c] > 2
+        }
+        enable = self.has_2 - self.active
+        for coords, delta in ((disable, -1), (enable, 1)):
+            for c in coords:
+                self.toggle(c, delta=delta)
 
 
 def solve() -> tuple[int, int]:
-    _puzzle = parse(read_puzzle())
-    return 0, 0
+    st = State.from_str(read_puzzle())
+    part1 = len(st.active)
+    for _ in range(100):
+        st.cycle()
+    part2 = len(st.active)
+    return part1, part2
 
 
 if __name__ == '__main__':
--- year2020/tests/test_aoc24.py
+++ year2020/tests/test_aoc24.py
@@ -1,5 +1,46 @@
-from year2020.aoc24 import solve
+from inspect import cleandoc
+
+from year2020.aoc24 import State, around, solve
+
+sample = cleandoc("""
+    sesenwnenenewseeswwswswwnenewsewsw
+    neeenesenwnwwswnenewnwwsewnenwseswesw
+    seswneswswsenwwnwse
+    nwnwneseeswswnenewneswwnewseswneseene
+    swweswneswnenwsewnwneneseenw
+    eesenwseswswnenwswnwnwsewwnwsene
+    sewnenenenesenwsewnenwwwse
+    wenwwweseeeweswwwnwwe
+    wsweesenenewnwwnwsenewsenwwsesesenwne
+    neeswseenwwswnwswswnw
+    nenwswwsewswnenenewsenwsenwnesesenew
+    enewnwewneswsewnwswenweswnenwsenwsw
+    sweneswneswneneenwnewenewwneswswnese
+    swwesenesewenwneswnwwneseswwne
+    enesenwswwswneneswsenwnewswseenwsese
+    wnwnesenesenenwwnenwsewesewsesesew
+    nenewswnwewswnenesenwnesewesw
+    eneswnwswnwsenenwnwnwwseeswneewsenese
+    neswnwewnwnwseenwseesewsenwsweewe
+    wseweeenwnesenwwwswnew
+""")
+
+
+def test_around():
+    assert around((1, 1)) == ((2, 1), (1, 2), (0, 2), (0, 1), (1, 0), (2, 0))
+
+
+def test_from_str():
+    st = State.from_str(sample)
+    assert len(st.active) == 10
+
+
+def test_cycle():
+    st = State.from_str(sample)
+    for n in (15, 12, 25, 14, 23, 28, 41, 37, 49, 37):
+        st.cycle()
+        assert len(st.active) == n
 
 
 def test_solve():
-    assert solve() == (0, 0)
+    assert solve() == (411, 4092)
