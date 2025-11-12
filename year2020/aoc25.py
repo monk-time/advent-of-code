c60bba2d31f3deb39bc5df350eabb6d260072c1e
Dmitry <monk-time@users.noreply.github.com>
2025-11-13 02:15:21 +0300

    Day 25

--- year2020/aoc25.py
+++ year2020/aoc25.py
@@ -2,14 +2,27 @@
 
 from helpers import read_puzzle
 
+MOD = 20201227
+
 
 def parse(s: str) -> list[int]:
     return [int(line) for line in s.split()]
 
 
+def find_lsize(pub: int) -> int:
+    n = loop_size = 1
+    while (n := (n * 7) % MOD) != pub:
+        loop_size += 1
+    return loop_size
+
+
+def get_enc_key(pub_card: int, pub_door: int) -> int:
+    return pow(pub_door, find_lsize(pub_card), MOD)
+
+
 def solve() -> tuple[int, int]:
-    _puzzle = parse(read_puzzle())
-    return 0, 0
+    pub_card, pub_door = parse(read_puzzle())
+    return get_enc_key(pub_card, pub_door), 0
 
 
 if __name__ == '__main__':
--- year2020/tests/test_aoc25.py
+++ year2020/tests/test_aoc25.py
@@ -1,5 +1,14 @@
-from year2020.aoc25 import solve
+from year2020.aoc25 import find_lsize, get_enc_key, solve
+
+
+def test_find_lsize():
+    assert find_lsize(5764801) == 8
+    assert find_lsize(17807724) == 11
+
+
+def test_get_enc_key():
+    assert get_enc_key(5764801, 17807724) == 14897079
 
 
 def test_solve():
-    assert solve() == (0, 0)
+    assert solve() == (9714832, 0)
